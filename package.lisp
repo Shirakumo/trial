ba9b88e9f11f236a1dd15f8e6efeaa377ba236f3
Shinmera <shinmera@tymoon.eu>
2016-03-20 12:59:52 +0100

    Fix header

--- package.lisp
+++ package.lisp
@@ -1,7 +1,7 @@
 #|
-This file is a part of simple-tasks
-(c) 2015 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
-Author: Nicolas Hafner <shinmera@tymoon.eu>
+ This file is a part of simple-tasks
+ (c) 2016 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
 |#
 
 (in-package #:cl-user)
