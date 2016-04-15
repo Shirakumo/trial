346b307188c85b6d6f9ea1b51ae8aa7285f5fd9b
Shinmera <shinmera@tymoon.eu>
2016-04-15 10:10:11 +0200

    Better colour coercion, only paint selectable-subjects to selection-buffers.

--- selectable.lisp
+++ selectable.lisp
@@ -10,22 +10,37 @@
 (defvar *color-id-counter* 0)
 (defvar *color-id-map* (trivial-garbage:make-weak-hash-table :test 'eql :weakness :value))
 
-(defun register-object-color (object)
-  (let ((id (incf *color-id-counter*)))
-    (setf (gethash id *color-id-map*) object)
-    id))
-
-(defun color->object (color-id)
-  (etypecase color-id
-    ((unsigned-byte 32))
+(defun ensure-color (color)
+  (etypecase color
+    ((unsigned-byte 32) color)
+    (cons
+     (let ((id 0))
+       (setf (ldb (byte 8 0) id) (or (fourth color) 255))
+       (setf (ldb (byte 8 8) id) (third color))
+       (setf (ldb (byte 8 16) id) (second color))
+       (setf (ldb (byte 8 24) id) (first color))
+       id))
+    ((vector (unsigned-byte 8) 3)
+     (let ((id 0))
+       (setf (ldb (byte 8 8) id) 255)
+       (setf (ldb (byte 8 8) id) (aref color 2))
+       (setf (ldb (byte 8 16) id) (aref color 1))
+       (setf (ldb (byte 8 24) id) (aref color 0))
+       id))
     ((vector (unsigned-byte 8) 4)
      (let ((id 0))
-       (setf (ldb (byte 8 0) id) (aref color-id 3))
-       (setf (ldb (byte 8 8) id) (aref color-id 2))
-       (setf (ldb (byte 8 16) id) (aref color-id 1))
-       (setf (ldb (byte 8 24) id) (aref color-id 0))
-       (setf color-id id))))
-  (gethash color-id *color-id-map*))
+       (setf (ldb (byte 8 0) id) (aref color 3))
+       (setf (ldb (byte 8 8) id) (aref color 2))
+       (setf (ldb (byte 8 16) id) (aref color 1))
+       (setf (ldb (byte 8 24) id) (aref color 0))
+       id))))
+
+(defun register-object-color (object &optional (id (incf *color-id-counter*)))
+  (setf (gethash (ensure-color id) *color-id-map*) object)
+  id)
+
+(defun color->object (id)
+  (gethash (ensure-color id) *color-id-map*))
 
 (define-finalizable selection-buffer (framebuffer)
   ())
@@ -36,7 +51,11 @@
   (gl:disable :blend :texture-2d)
   (gl:enable :depth-test :cull-face :multisample :line-smooth :polygon-smooth)
   (with-pushed-matrix
-      (paint scene buffer)))
+    (paint scene buffer)))
+
+(defmethod paint :around (thing (buffer selection-buffer))
+  (when (typep thing 'selectable-subject)
+    (call-next-method)))
 
 (defmethod object-at-point ((buffer selection-buffer) x y)
   (with-framebuffer-bound (buffer)
