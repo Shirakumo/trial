bffcf00cb3d6833a19d7b974596b94d254feedf5
Shinmera <shinmera@tymoon.eu>
2016-06-28 23:41:07 +0200

    Remove common code between controller and renderer

--- controller.lisp
+++ controller.lisp
@@ -9,61 +9,9 @@
 
 (define-subject controller ()
   (;; Has to be a double to avoid bignums after ~3.8 hours of runtime.
-   (tick-count :initform 0.0d0 :accessor tick-count)
-   (update-thread :initform NIL :accessor update-thread)
-   (last-pause :initform 0 :accessor last-pause)
-   (fps :initarg :fps :accessor fps)
-   (display :initarg :display :accessor display))
+   (tick-count :initform 0.0d0 :accessor tick-count))
   (:default-initargs
-   :name :controller
-   :fps 30.0f0
-   :display (error "DISPLAY required.")))
-
-(defmethod enter :after ((controller controller) (scene scene))
-  (setf (update-thread controller)
-        (with-thread ("controller update-thread")
-          (update-loop controller))))
-
-(defmethod finalize :after ((controller controller))
-  (let ((thread (update-thread controller)))
-    (with-thread-exit (thread)
-      (setf (update-thread controller) NIL))))
-
-(defun pause-time (fps start)
-  (let* ((duration (max 0 (- (current-time) start)))
-         (remainder (- (/ fps) (/ duration *time-units*))))
-    (max 0 remainder)))
-
-(defmacro with-frame-pause ((fps) &body body)
-  (let ((pause (gensym "PAUSE"))
-        (start (gensym "START")))
-    `(let ((,start (current-time)))
-       ,@body
-       (let ((,pause (pause-time ,fps ,start)))
-         (sleep ,pause)
-         ,pause))))
-
-(defun update-loop (controller)
-  (let* ((display (display controller))
-         (scene (scene display)))
-    (v:info :test "~a ~a" display *context*)
-    (with-slots-bound (controller controller)
-      (unwind-protect
-           (loop while (update-thread controller)
-                 do (with-simple-restart (abort "Abort the update and retry.")
-                      (setf (last-pause controller)
-                            (with-frame-pause ((fps controller))
-                              ;; Potentially release context every time to allow
-                              ;; other threads to grab it.
-                              (with-context (display :reentrant T)
-                                (issue scene 'tick :tick-count (tick-count controller))
-                                (process scene)
-                                (render display display)
-                                (render-hud display display)
-                                (q+:swap-buffers display))))))
-        
-        (stop scene))))
-  (v:info :trial.controller "Exiting update-loop."))
+   :name :controller))
 
 (defmethod render-hud ((controller controller) (display display))
   #+trial-debug-selection-buffer
