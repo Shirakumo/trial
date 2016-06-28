93151e740145018567fd3fdfc81bb5263f1b4636
Shinmera <shinmera@tymoon.eu>
2016-06-28 22:38:16 +0200

    Revised input system to be pluggable rather than directed to *main* or directly hooking into DISPLAY, making it completely independent.

--- input.lisp
+++ input.lisp
@@ -25,18 +25,6 @@
 (defclass key-release (keyboard-event)
   ())
 
-(define-override (display key-press-event) (ev)
-  (unless (q+:is-auto-repeat ev)
-    (let ((key (qt-key->symbol (q+:key ev))))
-      (v:debug :trial.input "Key pressed: ~a" key)
-      (issue (scene display) 'key-press :key key))))
-
-(define-override (display key-release-event) (ev)
-  (unless (q+:is-auto-repeat ev)
-    (let ((key (qt-key->symbol (q+:key ev))))
-      (v:debug :trial.input "Key release: ~a" key)
-      (issue (scene display) 'key-release :key key))))
-
 (defclass mouse-event (input-event)
   ((pos :initarg :pos :reader pos))
   (:default-initargs
@@ -65,24 +53,6 @@
 (defmethod print-object ((event mouse-move) stream)
   (print-unreadable-object (event stream :type T)
     (format stream "~a => ~a" (old-pos event) (pos event))))
-
-(define-override (display mouse-press-event) (ev)
-  (issue (scene display) 'mouse-press
-         :button (qt-button->symbol (q+:button ev))
-         :pos (vec (q+:x (q+:pos-f ev)) (q+:y (q+:pos-f ev)) 0)))
-
-(define-override (display mouse-release-event) (ev)
-  (issue (scene display) 'mouse-release
-         :button (qt-button->symbol (q+:button ev))
-         :pos (vec (q+:x (q+:pos-f ev)) (q+:y (q+:pos-f ev)) 0)))
-
-(defvar *previous-mouse-position* NIL)
-(define-override (display mouse-move-event) (ev)
-  (let ((pos (vec (q+:x (q+:pos-f ev)) (q+:y (q+:pos-f ev)) 0)))
-    (issue (scene display) 'mouse-move
-           :old-pos (or *previous-mouse-position* pos)
-           :pos pos)
-    (setf *previous-mouse-position* pos)))
 
 (defclass gamepad-event (input-event)
   ((device :initarg :device :reader device))
@@ -114,39 +84,112 @@
    :old-pos (error "OLD-POS required.")
    :pos (error "POS required.")))
 
-;; FIXME: Currently only the *MAIN* window can ever receive
-;;        gamepad input events. This might be fine in most
-;;        cases, but sometimes one might also want to receive
-;;        the events in another window. What if you have, say,
-;;        an input debugging dialogue or something like that?
+(defvar *input-handlers* ())
+(defvar *input-handlers-lock* (bt:make-lock))
+(defvar *input-gamepad-thread* ())
+
+;; Yes this is not really a widget, but the macro provides more convenience.
+(define-widget input-handler (QObject handler-container)
+  ((previous-pos :initform NIL)))
+
+(define-override (input-handler event-filter) (object ev)
+  (qclass=case ev
+    (qkeyevent
+     (unless (q+:is-auto-repeat ev)
+       (let ((key (qt-key->symbol (q+:key ev))))
+         (qtenumcase (q+:type ev)
+           ((q+:qevent.key-press)
+            (v:debug :trial.input "Key pressed: ~a" key)
+            (issue object 'key-press :key key))
+           ((q+:qevent.key-release)
+            (v:debug :trial.input "Key released: ~a" key)
+            (issue object 'key-release :key key))))))
+    (qmouseevent
+     (let ((button (qt-button->symbol (q+:button ev)))
+           (position (vec (q+:x (q+:pos-f ev)) (q+:y (q+:pos-f ev)) 0)))
+       (qtenumcase (q+:type ev)
+         ((q+:qevent.mouse-button-press)
+          (v:debug :trial.input "Mouse pressed: ~a" button)
+          (issue object 'mouse-press :button button :pos position))
+         ((q+:qevent.mouse-button-release)
+          (v:debug :trial.input "Mouse released: ~a" button)
+          (issue object 'mouse-release :button button :pos position))
+         ((q+:qevent.mouse-move)
+          (issue object 'mouse-move :old-pos (or previous-pos position) :pos position)
+          (setf previous-pos position))))))
+  (stop-overriding))
+
+(defmethod add-handler :after ((handlers list) (input-handler input-handler))
+  (dolist (handler handlers)
+    (q+:install-event-filter handler input-handler)))
+
+(defmethod add-handler :after (handler (input-handler input-handler))
+  (q+:install-event-filter handler input-handler))
+
+(defmethod remove-handler :after ((handlers list) (input-handler input-handler))
+  (dolist (handler handlers)
+    (q+:remove-event-filter handler input-handler)))
+
+(defmethod remove-handler :after (handler (input-handler input-handler))
+  (q+:remove-event-filter handler input-handler))
+
+;; All of this crap is necessary to allow distributing the gamepad events onto
+;; multiple instances of input-handlers with potentially multiple targets.
+(define-initializer (input-handler setup)
+  (bt:with-lock-held (*input-handlers-lock*)
+    (push input-handler *input-handlers*)))
+
+(define-finalizer (input-handler teardown)
+  (bt:with-lock-held (*input-handlers-lock*)
+    (setf *input-handlers* (remove input-handler *input-handlers*))))
+
+(defun init-input-system ()
+  (or *input-gamepad-thread*
+      (setf *input-gamepad-thread*
+            (with-thread ("gamepad event thread")
+              (cl-gamepad:init)
+              (unwind-protect
+                   (loop for i = 0 then (1+ i)
+                         while *input-gamepad-thread*
+                         do (when (= 0 (mod i 60))
+                              (cl-gamepad:detect-devices))
+                            (cl-gamepad:process-events)
+                            (sleep 1/60))
+                (cl-gamepad:shutdown))))))
+
+(defun shutdown-init-system ()
+  (with-thread-exit (*input-gamepad-thread*)
+    (setf *input-gamepad-thread* NIL)))
+
+(init-input-system)
+(pushnew #'shutdown-input-system qtools:*build-hooks*)
+(pushnew #'init-input-system qtools:*boot-hooks*)
+(pushnew #'shutdown-input-system qtools:*quit-hooks*)
 
 (defun cl-gamepad:device-attached (device)
   (v:info :trial.input "Attached ~s" (cl-gamepad:print-device device NIL))
-  (issue (scene *main*) 'gamepad-attach
-         :device device))
+  (dolist (handler *input-handlers*)
+    (handle (make-instance 'gamepad-attach :device device) handler)))
 
 (defun cl-gamepad:device-removed (device)
   (v:info :trial.input "Removed ~s" (cl-gamepad:print-device device NIL))
-  (issue (scene *main*) 'gamepad-remove
-         :device device))
+  (dolist (handler *input-handlers*)
+    (handle (make-instance 'gamepad-remove :device device) handler)))
 
 (defun cl-gamepad:button-pressed (button time device)
   (declare (ignore time))
-  (issue (scene *main*) 'gamepad-press
-         :button (gamepad-button->symbol device button)
-         :device device))
+  (let ((button (gamepad-button->symbol device button)))
+    (dolist (handler *input-handlers*)
+      (handle (make-instance 'gamepad-press :button button :device device) handler))))
 
 (defun cl-gamepad:button-released (button time device)
   (declare (ignore time))
-  (issue (scene *main*) 'gamepad-release
-         :button (gamepad-button->symbol device button)
-         :device device))
+  (let ((button (gamepad-button->symbol device button)))
+    (dolist (handler *input-handlers*)
+      (handle (make-instance 'gamepad-release :button button :device device) handler))))
 
 (defun cl-gamepad:axis-moved (axis last-value value time device)
   (declare (ignore time))
-  (issue (scene *main*) 'gamepad-move
-         :axis (gamepad-axis->symbol device axis)
-         :old-pos last-value
-         :pos value
-         :device device))
-
+  (let ((axis (gamepad-axis->symbol device axis)))
+    (dolist (handler *input-handlers*)
+      (handle (make-instance 'gamepad-move :axis axis :old-pos last-value :pos value :device device) handler))))
