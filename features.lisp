a388960e21f7ab9b9289f95422c92c850a0a6cb2
Shinmera <shinmera@tymoon.eu>
2016-08-30 00:41:55 +0200

    Make feature changes actually grip at compile time too.

--- features.lisp
+++ features.lisp
@@ -7,24 +7,25 @@
 (in-package #:org.shirakumo.fraf.trial)
 (in-readtable :qtools)
 
-(defvar *debug-features* '(:trial-debug-selection-buffer
-                           :trial-debug-bound-subject
-                           :trial-debug-error-on-invalid-asset-reference
-                           :trial-debug-controller))
-(defvar *optimize-features* '(:trial-optimize-resource-validity-check
-                              :trial-optimize-pool-watching))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *debug-features* '(:trial-debug-selection-buffer
+                             :trial-debug-bound-subject
+                             :trial-debug-error-on-invalid-asset-reference
+                             :trial-debug-controller))
+  (defvar *optimize-features* '(:trial-optimize-resource-validity-check
+                                :trial-optimize-pool-watching))
 
-#+trial-debug-all
-(setf *features* (union *features* *debug-features*))
+  #+trial-debug-all
+  (setf *features* (union *features* *debug-features*))
 
-#+trial-debug-none
-(setf *features* (set-difference *features* *debug-features*))
+  #+trial-debug-none
+  (setf *features* (set-difference *features* *debug-features*))
 
-#+trial-optimize-all
-(setf *features* (union *features* *optimize-features*))
+  #+trial-optimize-all
+  (setf *features* (union *features* *optimize-features*))
 
-#+trial-optimize-none
-(setf *features* (set-difference *features* *optimize-features*))
+  #+trial-optimize-none
+  (setf *features* (set-difference *features* *optimize-features*)))
 
 (defun reload-with-features (&rest features)
   (setf *features* (union *features* features))
