58eb649d506c0142b001d32b8035739d74e0c559
Shinmera <shinmera@tymoon.eu>
2017-03-16 18:30:34 +0100

    Initialise color to full white on fragment shader.

--- shader-subject.lisp
+++ shader-subject.lisp
@@ -114,4 +114,9 @@
   "#version 330 core")
 
 (define-class-shader shader-subject :fragment-shader
-  "#version 330 core")
+  "#version 330 core
+out vec4 color;
+
+void main(){
+  color = vec4(1.0, 1.0, 1.0, 1.0);
+}")
