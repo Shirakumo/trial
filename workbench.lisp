0767549b97479b20bca6f4ad0ded0690b999d5d9
Shinmera <shinmera@tymoon.eu>
2017-03-20 10:44:10 +0100

    Update to use global assets (untested)

--- workbench.lisp
+++ workbench.lisp
@@ -1,19 +1,36 @@
 (in-package #:trial)
 (in-readtable :qtools)
 
+(define-asset texture-asset (radiance av)
+              #p"~/av.png")
+
+(define-asset packed-vao-asset (radiance cube)
+              (#(0 1 2 2 3 0
+                 0 1 5 5 4 0
+                 2 3 7 7 6 2
+                 4 5 6 6 7 4)
+                3 #(+0.5 +0.5 +0.5
+                    +0.5 -0.5 +0.5
+                    -0.5 -0.5 +0.5
+                    -0.5 +0.5 +0.5
+                    +0.5 +0.5 -0.5
+                    +0.5 -0.5 -0.5
+                    -0.5 -0.5 -0.5
+                    -0.5 +0.5 -0.5)
+                2 #(1.0 1.0
+                    1.0 0.0
+                    0.0 0.0
+                    0.0 1.0
+                    1.0 1.0
+                    1.0 0.0
+                    0.0 0.0
+                    0.0 1.0)))
+
 (define-shader-subject testcube (vertex-subject textured-subject)
   ()
   (:default-initargs
-   :texture (make-asset 'texture-asset #p"~/av.png")
-   :vertex-array (pack-vao #(3 1 0 3 2 1)
-                  3 #(+0.5 +0.5 0.0
-                      +0.5 -0.5 0.0
-                      -0.5 -0.5 0.0
-                      -0.5 +0.5 0.0)
-                  2 #(1.0 1.0
-                      1.0 0.0
-                      0.0 0.0
-                      0.0 1.0))))
+   :texture (asset 'radiance 'av)
+   :vertex-array (asset 'radiance 'cube)))
 
 (defmethod setup-scene ((main main))
   (let ((scene (scene main)))
@@ -32,27 +49,4 @@
     (rotate +vx+ 0.03)
     (rotate +vy+ 0.05)
     (rotate +vz+ 0.07)
-    (with-pushed-matrix
-      (translate-by 0 0 -0.5)
-      (rotate +vy+ (* pi 1/180 180))
-      (paint scene target))
-    (with-pushed-matrix
-      (translate-by 0 0 0.5)
-      (rotate +vy+ (* pi 1/180 0))
-      (paint scene target))
-    (with-pushed-matrix
-      (translate-by -0.5 0 0)
-      (rotate +vy+ (* pi 1/180 -90))
-      (paint scene target))
-    (with-pushed-matrix
-      (translate-by 0.5 0 0)
-      (rotate +vy+ (* pi 1/180 90))
-      (paint scene target))
-    (with-pushed-matrix
-      (translate-by 0 0.5 0)
-      (rotate +vx+ (* pi 1/180 -90))
-      (paint scene target))
-    (with-pushed-matrix
-      (translate-by 0 -0.5 0)
-      (rotate +vx+ (* pi 1/180 90))
-      (paint scene target))))
+    (paint scene target)))
