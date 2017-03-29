208c236a3b34b94e95233fbee511207706154848
Shinmera <shinmera@tymoon.eu>
2017-03-29 10:43:08 +0200

    Fix negative pass

--- effects.lisp
+++ effects.lisp
@@ -18,7 +18,7 @@ uniform sampler2D previousPass;
 
 void main(){
   color = texture(previousPass, texCoord);
-  color = vec4(1.0-color.x, 1.0-color.y, 1.0-color.z, 1.0);
+  color = vec4(1.0-color.x, 1.0-color.y, 1.0-color.z, color.w);
 }")
 
 (define-shader-pass grayscale-pass (post-effect-pass)
