ff10819b6e8f09f9a523ecd431bef66876ed2066
Shinmera <shinmera@tymoon.eu>
2017-06-15 23:35:01 +0200

    Simple FPS workbench

--- workbench.lisp
+++ workbench.lisp
@@ -3,37 +3,18 @@
 (define-pool workbench
   :base 'trial)
 
-(define-asset (workbench cat) texture
-    (#p"cat.png"))
-
-(define-asset (workbench teapot) mesh
-    (#p"teapot.vf")
-  :mesh :Teapot01Mesh)
-
 (define-asset (workbench noto) font
-    (#p"noto-sans-regular.ttf")
-  :charset "(°<)")
+    (#p"noto-sans-regular.ttf"))
 
-(progn
-  (define-shader-subject teapot (vertex-subject textured-subject located-entity rotated-entity)
-    ()
-    (:default-initargs
-     :vertex-array (asset 'workbench 'teapot)
-     :texture (asset 'workbench 'cat)))
-  
-  (maybe-reload-scene))
-
-(define-handler (teapot tick) (ev)
-  (incf (vz (rotation teapot)) (/ (random 1.0) 10))
-  (incf (vx (rotation teapot)) (/ (random 1.0) 10))
-  (incf (vy (rotation teapot)) (/ (random 1.0) 10)))
+(defmethod paint :before ((source main) (target main))
+  (let ((fps (unit :fps (scene source))))
+    (when fps
+      (setf (text fps) (format NIL "~a" (round (/ (frame-time source))))))))
 
 (progn
   (defmethod setup-scene ((main main))
     (let ((scene (scene main)))
-      ;; (dotimes (i 200)
-      ;;   (enter (make-instance 'teapot :location (vec3-random -200 200)) scene))
-      (enter (make-instance 'text :font (asset 'workbench 'noto) :text "(°<°)") scene)
+      (enter (make-instance 'text :font (asset 'workbench 'noto) :name :fps) scene)
       (enter (make-instance 'target-camera :location (vec 0 2 200)) scene)))
 
   (maybe-reload-scene))
@@ -41,12 +22,7 @@
 (progn
   (defmethod setup-pipeline ((main main))
     (let ((pipeline (pipeline main))
-          (pass1 (make-instance 'render-pass))
-          (pass2 (make-instance 'negative-pass))
-          (pass3 (make-instance 'box-blur-pass)))
-      (register pass1 pipeline)
-      ;; (connect (flow:port pass1 'color) (flow:port pass2 'previous-pass) pipeline)
-      ;; (connect (flow:port pass2 'color) (flow:port pass3 'previous-pass) pipeline)
-      ))
+          (pass1 (make-instance 'render-pass)))
+      (register pass1 pipeline)))
 
   (maybe-reload-scene))
