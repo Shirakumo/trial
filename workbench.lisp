5c356c3d323c7aaf3476e578db5a90797a79989d
Shinmera <shinmera@tymoon.eu>
2017-06-17 09:54:04 +0200

    Clean workbench

--- workbench.lisp
+++ workbench.lisp
@@ -3,20 +3,10 @@
 (define-pool workbench
   :base 'trial)
 
-(define-asset (workbench noto) font
-    (#p"noto-sans-regular.ttf"))
-
-(defmethod paint :before ((source main) (target main))
-  (let ((fps (unit :fps (scene source)))
-        (tt (frame-time source)))
-    (when (and fps (/= 0 tt))
-      (setf (text fps) (format NIL "~a" (round (/ tt)))))))
-
 (progn
   (defmethod setup-scene ((main main))
     (let ((scene (scene main)))
-      (enter (make-instance 'text :font (asset 'workbench 'noto) :name :fps) scene)
-      (enter (make-instance 'target-camera :location (vec 0 2 200)) scene)))
+      ))
 
   (maybe-reload-scene))
 
