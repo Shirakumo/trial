37ff9a99f6e259804cd8a640d7edbc6b3bd862a2
gingeralesy <gingeralesy@gmail.com>
2017-06-26 13:07:23 +0300

    Fixes here and there

--- physics/verlet.lisp
+++ physics/verlet.lisp
@@ -8,7 +8,7 @@ Author: Janne Pakarinen <gingeralesy@gmail.com>
 
 (defvar *default-forces* (vec 0 0.5)
   "Downways.")
-(defvar *default-viscosity* 1.0d0
+(defvar *default-viscosity* 1.0
   "How hard is it to move horizontally when falling on a surface.")
 (defvar *iterations* 5
   "Number of physics calculation iterations. Increases accuracy of calculations.")
@@ -27,7 +27,7 @@ Author: Janne Pakarinen <gingeralesy@gmail.com>
 
 (defclass physical-point ()
   ((location :initarg :location :accessor location)
-   (old-location :initform NIL :accessor location)
+   (old-location :initform NIL :accessor old-location)
    (acceleration :initform NIL :accessor acceleration))
   (:default-initargs :location (error "Must define a location for a point!")))
 
@@ -118,8 +118,8 @@ Author: Janne Pakarinen <gingeralesy@gmail.com>
             (length = (vlength a-to-b))
             (diff = (- length (original-length edge)))
             (normal = (vnormal a-to-b)))
-    (setf (location (point-a edge)) (v+ point-a (v* normal diff 0.5d0))
-          (location (point-b edge)) (v- point-b (v* normal diff 0.5d0)))))
+    (setf (location (point-a edge)) (v+ point-a (v* normal diff 0.5))
+          (location (point-b edge)) (v- point-b (v* normal diff 0.5)))))
 
 (defmethod project-to-axis ((entity physical-entity) axis)
   "Gets the nearest and furthest point along an axis." ;; Think of it like casting a shadow on a wall.
@@ -166,8 +166,8 @@ vertex: point that pierces furthest in"
              (ent2 (if (eql (parent col-edge) other) other entity))
              (center (v- (center ent1) (center ent2))) ;; Already calculated in update-physics
              (sign (v. normal center))
-             (mass1 (cond ((static-p ent1) 0.0d0) ((static-p ent2) 1.0d0) (T (mass ent1))))
-             (mass2 (cond ((static-p ent2) 0.0d0) ((static-p ent1) 1.0d0) (T (mass ent2))))
+             (mass1 (cond ((static-p ent1) 0.0) ((static-p ent2) 1.0) (T (mass ent1))))
+             (mass2 (cond ((static-p ent2) 0.0) ((static-p ent1) 1.0) (T (mass ent2))))
              (total-mass (+ mass1 mass2))
              (smallest-dist)
              (vertex))
@@ -184,33 +184,36 @@ vertex: point that pierces furthest in"
 
 (defun resolve-collision (depth mass-a mass-b normal edge vertex)
   "Pushes back the two entities from one another. The normal always points towards the piercing entity."
-  (let ((response (v* normal depth))) ;; Pushback for the piercing entity
-    (setf (location vertex) (v+ (location vertex) (v* response mass-a)))
-    (let* ((point-a (location (point-a edge)))
-           (point-b (location (point-b edge))) ;; Pushback for the edging entity
-           ;; t-point is the factor that determines where on the edge the vertex lies, [0, 1]
-           ;; It has to do the if-else check so we don't accidentally divide by zero
-           (t-point (if (< (abs (- (vy point-a) (vy point-b))) (abs (- (vx point-a) (vx point-b))))
-                        (/ (- (vx (location vertex)) (vx response) (vx point-a))
-                           (- (vx point-b) (vx point-a)))
-                        (/ (- (vy (location vertex)) (vy response) (vy point-a))
-                           (- (vy point-b) (vy point-a)))))
-           ;; Now lambda here. It's the scaling factor for ensuring that the collision vertex lies on
-           ;; the collision edge. I have no idea who came up with it but it's just
-           ;; lambda = 1 / (t^2 + (1 - t)^2)
-           (lmba (/ (+ (* t-point t-point) (* (- 1 t-point) (- 1 t-point))))))
-      ;; And here we just reduce it for pushback
-      ;; Note the (- 1 t-point) and t-point multipliers.
-      ;; It causes a bit of spin if edge wasn't hit in the middle.
-      ;; ... I really hope the masses are right way around.
-      (setf (location (point-a edge)) (v- point-a (v* response (- 1 t-point) mass-b lmba))
-            (location (point-b edge)) (v- point-b (v* response t-point mass-b lmba))))))
+  (when (and depth mass-a mass-b normal edge vertex)
+    (let ((response (v* normal depth))) ;; Pushback for the piercing entity
+      (setf (location vertex) (v+ (location vertex) (v* response mass-a)))
+      (let* ((point-a (location (point-a edge)))
+             (point-b (location (point-b edge))) ;; Pushback for the edging entity
+             ;; t-point is the factor that determines where on the edge the vertex lies, [0, 1]
+             ;; It has to do the if-else check so we don't accidentally divide by zero
+             (t-point (if (< (abs (- (vy point-a) (vy point-b))) (abs (- (vx point-a) (vx point-b))))
+                          (/ (- (vx (location vertex)) (vx response) (vx point-a))
+                             (- (vx point-b) (vx point-a)))
+                          (/ (- (vy (location vertex)) (vy response) (vy point-a))
+                             (- (vy point-b) (vy point-a)))))
+             ;; Now lambda here. It's the scaling factor for ensuring that the collision vertex lies on
+             ;; the collision edge. I have no idea who came up with it but it's just
+             ;; lambda = 1 / (t^2 + (1 - t)^2)
+             (lmba (/ (+ (* t-point t-point) (* (- 1 t-point) (- 1 t-point))))))
+        ;; And here we just reduce it for pushback
+        ;; Note the (- 1 t-point) and t-point multipliers.
+        ;; It causes a bit of spin if edge wasn't hit in the middle.
+        ;; ... I really hope the masses are right way around.
+        ;; TODO: Should we consider kinetic energy as well as the mass?
+        ;;       We do not store velocity with verlet physics. Leave it for the force application?
+        (setf (location (point-a edge)) (v- point-a (v* response (- 1 t-point) mass-b lmba))
+              (location (point-b edge)) (v- point-b (v* response t-point mass-b lmba)))))))
 
 (defun update-physics (entities)
   (for:for ((entity in entities))
     (apply-forces entity)
     (update-edges entity))
-  (dotimes (i *physics-iterations*) ;; More you do it, better it gets
+  (dotimes (i *iterations*) ;; More you do it, better it gets
     (loop for list = entities then (rest list)
           for entity = (first list)
           for rest = (rest list)
@@ -220,6 +223,4 @@ vertex: point that pierces furthest in"
           do (for:for ((other in rest))
                (update-edges other)
                (calculate-center other)
-               (multiple-value-bind (depth mass-a mass-b normal edge vertex)
-                   (collides-p entity other)
-                 (when depth (resolve-collision depth mass-a mass-b normal edge vertex)))))))
+               (multiple-value-call #'resolve-collision (collides-p entity other))))))
