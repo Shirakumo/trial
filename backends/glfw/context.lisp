1ba3a288ce35a0f0b075335a7403c4ace27142b7
Shinmera <shinmera@tymoon.eu>
2017-07-31 14:57:47 +0200

    Change forward compat default to T

--- backends/glfw/context.lisp
+++ backends/glfw/context.lisp
@@ -41,7 +41,7 @@
                                           (visible NIL visible-p)
                                           (decorated NIL decorated-p)
                                           (robustness NIL robustness-p)
-                                          (forward-compat NIL forward-compat-p)
+                                          (forward-compat T forward-compat-p)
                                           (debug-context NIL debug-context-p))
   (flet (((setf g) (value name) (setf (getf (initargs context) name) value)))
     (macrolet ((maybe-set (var &optional (name (intern (string var) :keyword)))
