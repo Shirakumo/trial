f8340bdccdd08dde6c02bc160a0026bc46712e1f
Shinmera <shinmera@tymoon.eu>
2017-11-12 21:41:02 +0100

    Geometry clipmap shit, I don't know what's going on anymore and I feel like I've been wasting my time these past six!!!!!! weeks. GOOOOOD DANGJKAHNDWAK;kl

--- geometry-clipmap.lisp
+++ geometry-clipmap.lisp
@@ -38,29 +38,26 @@
     (setf (uniform shader "model_matrix") (model-matrix))
     (setf (uniform shader "view_matrix") (view-matrix))
     (setf (uniform shader "projection_matrix") (projection-matrix))
-    (setf (uniform shader "ViewerPos") (vec2 0 0))
-    (draw-ring clipmap m s shader 0 1.0 0 0)
+    (draw-ring clipmap m s shader 0 1.0 0 0 0)
     (loop for level from 1 below (slot-value clipmap 'levels)
           for scale = 0.5 then (/ scale 2)
+          for offp = 0 then off
           for off = (* s scale -1) then (+ off (* s scale))
-          do (draw-ring clipmap m s shader level scale off 1))))
+          do (draw-ring clipmap m s shader level scale off offp 1))))
 
-(defun draw-ring (clipmap m s shader level scale off f)
+(defun draw-ring (clipmap m s shader level scale off offp f)
   (setf (uniform shader "level") (float level 0s0))
-  (setf (uniform shader "ScaleFactor") (vec4 scale scale off off))
-  (setf (uniform shader "FineBlockOrig") (vec4 scale scale (+ 0.5 off) (+ 0.5 off)))
-  (setf (uniform shader "FinerBlockOrig") (vec4 1 1 (+ 0.5 off) (+ 0.5 off)))
+  (setf (uniform shader "scale") (float scale 0s0))
+  (setf (uniform shader "offset") (float off 0s0))
+  (setf (uniform shader "offsetp") (float offp 0s0))
+  (setf (uniform shader "block") (vec 0 0))
   (gl:bind-vertex-array (resource (clipmap-fixup clipmap)))
   (%gl:draw-elements :triangles (size (clipmap-fixup clipmap)) :unsigned-int 0)
-  (setf (uniform shader "ScaleFactor") (vec4 scale scale off off))
-  (setf (uniform shader "FineBlockOrig") (vec4 scale scale (+ 0.5 off) (+ 0.5 off)))
-  (setf (uniform shader "FinerBlockOrig") (vec4 1 1 (+ 0.5 off) (+ 0.5 off)))
   (gl:bind-vertex-array (resource (nth f (clipmap-trims clipmap))))
   (%gl:draw-elements :triangles (size (nth f (clipmap-trims clipmap))) :unsigned-int 0)
   (gl:bind-vertex-array (resource (clipmap-block clipmap)))
   (flet ((d (x z)
-           (setf (uniform shader "ScaleFactor") (vec4 scale scale (+ (* x scale) off) (+ (* z scale) off)))
-           (setf (uniform shader "FinerBlockOrig") (vec4 1 1 (+ 0.5 x off) (+ 0.5 z off)))
+           (setf (uniform shader "block") (vec x z))
            (%gl:draw-elements :triangles (size (clipmap-block clipmap)) :unsigned-int 0)))
     (d (- 0.5 (* s m 0.5)) (- 0.5 (* s m 0.5)))
     (d (- 0.5 (* s m 0.5)) (- 0.5 (* s m 1.5)))
@@ -84,27 +81,27 @@ uniform mat4 model_matrix;
 uniform mat4 view_matrix;
 uniform mat4 projection_matrix;
 uniform sampler2DArray texture_image;
-uniform vec4 ScaleFactor, FineBlockOrig, FinerBlockOrig;
-uniform vec2 ViewerPos;
-uniform float level;
+uniform float level, scale, offset, offsetp;
+uniform vec2 block;
 
 out float z;
 out float a;
 out vec2 uv;
 
 void main(){
-   vec2 worldPos = position.xz * ScaleFactor.xy + ScaleFactor.zw;
-   vec2 uv_inner = position.xz * FinerBlockOrig.xy + FinerBlockOrig.zw;
-   vec2 uv_outer = uv_inner / 2; // THIS SHOULD WORK BUT DOES NOT WTFFBBQ
+   vec2 world = (position.xz + block) * scale + offset;
+   vec2 uv_inner = position.xz + 0.5 + block;
+   vec2 uv_outer = (position.xz + 0.5 + block)/2;
 
-   vec2 alpha = clamp(abs(worldPos/ScaleFactor.xy)*20-8, 0, 1);
+   vec2 alpha = clamp(abs(world/scale)*10-4, 0, 1);
    a = max(alpha.x, alpha.y);
+   if(level == 0) a = 0;
 
    float zi = texture(texture_image, vec3(uv_inner, level)).r;
    float zo = texture(texture_image, vec3(uv_outer, max(level-1, 0))).r;
    z = ((1-a) * zi + a * zo) * 0.5;
 
-   gl_Position = projection_matrix * view_matrix * model_matrix * vec4(worldPos.x, z, worldPos.y, 1.0);
+   gl_Position = projection_matrix * view_matrix * model_matrix * vec4(world.x, z, world.y, 1.0);
    uv = uv_inner;
 }")
 
@@ -117,13 +114,14 @@ out vec4 color;
 
 uniform vec3 LightDirection = vec3(0.2, -0.8, 0.2);
 uniform sampler2DArray texture_image;
+uniform float level;
 
 void main(){
 //    vec3 normal = vec3(0.2, 0.8, 0.2);
 //    float s = clamp(dot(normal, LightDirection), 0, 1);
 //    color = s * vec4(max(1.0, z+0.2), 0, 0, 1);
-    color = texture(texture_image, vec3(uv, 0));
-    color.r += a;
+    color = texture(texture_image, vec3(uv, level))*2;
+    color.r = a;
 }")
 
 (defun make-clipmap-block (n)
--- workbench.lisp
+++ workbench.lisp
@@ -9,15 +9,14 @@
      #p"/home/linus/output3.png")
   :target :texture-2d-array
   :min-filter :linear
-  :wrapping :clamp)
+  :wrapping :clamp-to-edge)
 
 (progn
   (defmethod setup-scene ((main main))
     (let ((scene (scene main)))
-      (gl:polygon-mode :front-and-back :line)
+      (gl:polygon-mode :front-and-back :fill)
       (enter (make-instance 'clipmap :n 127 :levels 3 :texture (asset 'workbench 'heightmap)) scene)
       (enter (make-instance 'vertex-entity :vertex-array (make-asset 'mesh (list (make-cone 0.0001 0.5 :segments 16)))) scene)
-      (enter (make-instance 'vertex-entity :vertex-array (make-asset 'mesh (list (make-quad-grid 1 1 1)))) scene)
       ;; (enter (make-instance 'vertex-entity :vertex-array (make-asset 'mesh (list (make-quad-grid (/ 16) 4 2)))) scene)
       (enter (make-instance 'editor-camera :move-speed 0.001 :location (vec 0 0.2 0)) scene)
       ;;(enter (make-instance 'target-camera :target (vec 0 0 0) :location (vec 0 0.6 0.0000001)) scene)
