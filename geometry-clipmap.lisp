82d2faf45be2e412878678c47b8ebde7fb395ef9
Shinmera <shinmera@tymoon.eu>
2017-11-19 23:55:06 +0100

    More clipmaps shite.

--- geometry-clipmap.lisp
+++ geometry-clipmap.lisp
@@ -12,9 +12,11 @@
    (clipmap-block :accessor clipmap-block)
    (clipmap-fixup :accessor clipmap-fixup)
    (clipmap-trims :accessor clipmap-trims)
+   (clipmap-center :accessor clipmap-center)
    (texture :initarg :texture :accessor texture)))
 
 (defmethod initialize-instance :after ((clipmap clipmap) &key n)
+  (setf (clipmap-center clipmap) (make-asset 'mesh (list (make-quad-grid (/ (1- n)) (/ (1+ n) 2) (/ (1+ n) 2)))))
   (setf (clipmap-block clipmap) (make-asset 'mesh (list (make-clipmap-block n))))
   (setf (clipmap-fixup clipmap) (make-asset 'mesh (list (make-clipmap-fixup n))))
   (let ((trims (make-clipmap-trim n)))
@@ -22,6 +24,7 @@
                                       collect (make-asset 'mesh (list trim))))))
 
 (defmethod load progn ((clipmap clipmap))
+  (load (clipmap-center clipmap))
   (load (clipmap-block clipmap))
   (load (clipmap-fixup clipmap))
   (mapc #'load (clipmap-trims clipmap))
@@ -35,7 +38,6 @@
          (m (1- (/ (1+ n) 4)))
          (s (/ (1- n)))
          (shader (shader-program-for-pass pass clipmap)))
-    (setf (uniform shader "model_matrix") (model-matrix))
     (setf (uniform shader "view_matrix") (view-matrix))
     (setf (uniform shader "projection_matrix") (projection-matrix))
     (draw-ring clipmap m s shader 0 1.0 0 0 0)
@@ -43,7 +45,14 @@
           for scale = 0.5 then (/ scale 2)
           for offp = 0 then off
           for off = (* s scale -1) then (+ off (* s scale))
-          do (draw-ring clipmap m s shader level scale off offp 1))))
+          do (draw-ring clipmap m s shader level scale off offp 1)
+          finally   (setf (uniform shader "level") (float level 0s0))
+                    (setf (uniform shader "scale") (float scale 0s0))
+                    (setf (uniform shader "offset") (float off 0s0))
+                    (setf (uniform shader "offsetp") (float offp 0s0))
+                    (setf (uniform shader "block") (vec 0 0))
+                    (gl:bind-vertex-array (resource (clipmap-center clipmap)))
+                    (%gl:draw-elements :triangles (size (clipmap-center clipmap)) :unsigned-int 0))))
 
 (defun draw-ring (clipmap m s shader level scale off offp f)
   (setf (uniform shader "level") (float level 0s0))
@@ -77,52 +86,79 @@
 (define-class-shader (clipmap :vertex-shader)
   "layout (location = 0) in vec3 position;
 
-uniform mat4 model_matrix;
 uniform mat4 view_matrix;
 uniform mat4 projection_matrix;
 uniform sampler2DArray texture_image;
 uniform float level, scale, offset, offsetp;
 uniform vec2 block;
 
+out vec3 normal;
 out float z;
-out float a;
-out vec2 uv;
 
 void main(){
+   float level_o = max(level-1, 0);
    vec2 world = (position.xz + block) * scale + offset;
-   vec2 uv_inner = position.xz + 0.5 + block + offset;
-   vec2 uv_outer = (position.xz + 1 + block - offsetp)/2 + offset;
+   vec2 uv_inner = position.xz + block + offset + 0.5;
+   vec2 uv_outer = (position.xz + block - offsetp)/2 + offset + 0.5;
 
    vec2 alpha = clamp(abs(world/scale)*10-3.8, 0, 1);
-   a = max(alpha.x, alpha.y);
+   float a = max(alpha.x, alpha.y);
    if(level == 0) a = 0;
 
    float zi = texture(texture_image, vec3(uv_inner, level)).r;
-   float zo = texture(texture_image, vec3(uv_outer, max(level-1, 0))).r;
-   z = ((1-a) * zi + a * zo) * 0.5;
+   float zo = texture(texture_image, vec3(uv_outer, level_o)).r;
+   z = ((1-a) * zi + a * zo) * 0.3;
 
-   gl_Position = projection_matrix * view_matrix * model_matrix * vec4(world.x, z, world.y, 1.0);
-   uv = uv_inner;
+   gl_Position = projection_matrix * view_matrix * vec4(world.x, z, world.y, 1.0);
+
+   // deduce terrain normal
+   vec3 off = vec3(0.0078125, 0.0078125, 0.0);
+   float hLi = texture(texture_image, vec3(uv_inner - off.xz, level)).r*100;
+   float hRi = texture(texture_image, vec3(uv_inner + off.xz, level)).r*100;
+   float hDi = texture(texture_image, vec3(uv_inner - off.zy, level)).r*100;
+   float hUi = texture(texture_image, vec3(uv_inner + off.zy, level)).r*100;
+   float hLo = texture(texture_image, vec3(uv_outer - off.xz, level_o)).r*100;
+   float hRo = texture(texture_image, vec3(uv_outer + off.xz, level_o)).r*100;
+   float hDo = texture(texture_image, vec3(uv_outer - off.zy, level_o)).r*100;
+   float hUo = texture(texture_image, vec3(uv_outer + off.zy, level_o)).r*100;
+   float hL = ((1-a) * hLi + a * hLo);
+   float hR = ((1-a) * hRi + a * hRo);
+   float hD = ((1-a) * hDi + a * hDo);
+   float hU = ((1-a) * hUi + a * hUo);
+
+   normal.x = hL - hR;
+   normal.y = hD - hU;
+   normal.z = 2.0;
+   normal = normalize(normal);
 }")
 
 (define-class-shader (clipmap :fragment-shader)
   "
+uniform vec3 light = vec3(0.5, 1.0, 2.0);
+uniform sampler2DArray texture_image;
+
+in vec3 normal;
 in float z;
-in float a;
-in vec2 uv;
 out vec4 color;
 
-uniform vec3 LightDirection = vec3(0.2, -0.8, 0.2);
-uniform sampler2DArray texture_image;
-uniform float level;
-
 void main(){
-//    vec3 normal = vec3(0.2, 0.8, 0.2);
-//    float s = clamp(dot(normal, LightDirection), 0, 1);
-//    color = s * vec4(max(1.0, z+0.2), 0, 0, 1);
-    color = texture(texture_image, vec3(uv, level))*2;
-    color.r = a;
+    float v = z*3+abs(normal.y)/2.5;
+    float s = clamp(dot(normal, normalize(light)), 0, 1);
+    vec3 c = vec3(1,1,1);
+    if(v < 0.5){
+      c = vec3(0.2, 0.7, 0.2);
+    }else if(v < 0.75){
+      c = mix(vec3(0.2, 0.7, 0.2), vec3(0.6,0.6,0.6), (v-0.5)/0.25);
+    }else if(v < 0.9){
+      c = mix(vec3(0.6,0.6,0.6), vec3(1.0,1.0,1.0), (v-0.75)/0.15);
+    }else{
+      c = vec3(1,1,1);
+    }
+    color = vec4(s * c, 1.0);
 }")
+
+(setf (location (unit :camera (scene (window :main))))
+      (vec 0 0.28 0))
 
 (defun make-clipmap-block (n)
   (let ((m (1- (/ (1+ n) 4)))
--- workbench.lisp
+++ workbench.lisp
@@ -15,10 +15,9 @@
   (defmethod setup-scene ((main main))
     (let ((scene (scene main)))
       (gl:polygon-mode :front-and-back :fill)
-      (enter (make-instance 'clipmap :n 127 :levels 3 :texture (asset 'workbench 'heightmap)) scene)
-      (enter (make-instance 'vertex-entity :vertex-array (make-asset 'mesh (list (make-cone 0.0001 0.5 :segments 16)))) scene)
+      (enter (make-instance 'clipmap :n 255 :levels 3 :texture (asset 'workbench 'heightmap)) scene)
       ;; (enter (make-instance 'vertex-entity :vertex-array (make-asset 'mesh (list (make-quad-grid (/ 16) 4 2)))) scene)
-      (enter (make-instance 'editor-camera :move-speed 0.001 :location (vec 0 0.2 0)) scene)
+      (enter (make-instance 'editor-camera :move-speed 0.001 :location (vec 0 0.2 0) :name :camera) scene)
       ;;(enter (make-instance 'target-camera :target (vec 0 0 0) :location (vec 0 0.6 0.0000001)) scene)
       ))
 
