ae090f9bc11f60ec420177235166c416a91da1c5
Shinmera <shinmera@tymoon.eu>
2017-11-27 12:28:57 +0100

    Implement 1d array texture

--- assets/texture.lisp
+++ assets/texture.lisp
@@ -64,16 +64,18 @@
 
 (defun images-to-textures (target images)
   (case target
-    ;; FIXME: Array textures
     (:texture-cube-map
      (loop for image in images
            for target in '(:texture-cube-map-positive-x :texture-cube-map-negative-x
                            :texture-cube-map-positive-y :texture-cube-map-negative-y
                            :texture-cube-map-positive-z :texture-cube-map-negative-z)
            do (images-to-textures target (list image))))
+    (:texture-1d-array
+     (with-image-object (width height bits format) (first images)
+       (gl:tex-image-2d target 1 format width height 0 (texture-internal-format->texture-format format) (texture-format->data-type format) bits)))
     (:texture-2d-array
      (with-image-object (width height bits format) (first images)
-       (%gl:tex-storage-3d target 1 :rgba32f width height (length images))
+       (%gl:tex-storage-3d target 1 format width height (length images))
        (gl:tex-sub-image-3d target 0 0 0 0 width height 1 (texture-internal-format->texture-format format) (texture-format->data-type format) bits)
        (loop for level from 1
              for image in (rest images)
