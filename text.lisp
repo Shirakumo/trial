a1ba4df0ecf3b5e091d65c37a7c7b661d0b943b9
Shinmera <shinmera@tymoon.eu>
2018-03-14 18:14:32 +0100

    Implement primitive text wrapping.

--- text.lisp
+++ text.lisp
@@ -10,10 +10,16 @@
   ((texture :initarg :font :accessor font)
    (text :initarg :text :accessor text)
    (size :initarg :size :accessor size)
+   (wrap :initarg :wrap :accessor wrap)
+   (width :initarg :width :accessor width)
+   (height :initarg :height :accessor height)
    (vbo) (ebo))
   (:default-initargs
    :color (vec 0 0 0 1)
    :text ""
+   :wrap NIL
+   :width NIL
+   :height NIL
    :font (error "FONT required."))
   (:inhibit-shaders (colored-entity :fragment-shader)))
 
@@ -54,13 +60,57 @@ void main(){
 (defmethod resources-ready ((text text))
   (setf (text text) (text text)))
 
+(defun wrap-text (font string width)
+  ;; FIXME: This is really primitive.
+  (destructuring-bind (&key l r ((:t u)) b gap) (text-extent font " ")
+    (let ((s (+ r l))
+          (current-width 0)
+          (height (- u b))
+          (buffer (make-string-output-stream))
+          (vskip (/ (* (size font) (+ gap u)) (+ u b))))
+      (values (with-output-to-string (out)
+                (flet ((complete-word ()
+                         (let* ((word (get-output-stream-string buffer))
+                                (ext (text-extent font word))
+                                (w (+ (getf ext :r) (getf ext :l))))
+                           (when (<= width (+ current-width s w))
+                             (write-char #\Linefeed out)
+                             (setf current-width 0)
+                             (incf height vskip))
+                           (write-string word out)
+                           (write-char #\Space out)
+                           (incf current-width (+ s w)))))
+                  (loop for c across string
+                        do (case c
+                             (#\Space
+                              (complete-word))
+                             (#\Linefeed
+                              (setf current-width width)
+                              (complete-word))
+                             (T
+                              (write-char c buffer)))
+                        finally (complete-word))))
+              height))))
+
+(defmethod (setf width) :after (width (text text))
+  (when (wrap text)
+    (setf (text text) (text text))))
+
 (defmethod (setf text) :before (string (text text))
   (let ((vao (vertex-array text))
         (vbo (slot-value text 'vbo))
         (ebo (slot-value text 'ebo))
         (font (font text)))
     (when (allocated-p font)
-      (setf (size vao) (cl-fond:update-text font string (gl-name vbo) (gl-name ebo))))))
+      (if (wrap text) 
+          (multiple-value-bind (string height) (wrap-text font string (width text))
+            (setf (height text) height)
+            (setf (size vao) (cl-fond:update-text font string (gl-name vbo) (gl-name ebo))))
+          (destructuring-bind (&key l r ((:t u)) b gap) (text-extent text string)
+            (declare (ignore gap))
+            (setf (width text) (+ l r))
+            (setf (height text) (+ u b))
+            (setf (size vao) (cl-fond:update-text font string (gl-name vbo) (gl-name ebo))))))))
 
 (defmethod extent ((text text))
   (text-extent text (text text)))
