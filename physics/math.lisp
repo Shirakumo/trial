47d76a5fb97825a975dfa75ebf431ae201cca66e
gingeralesy <gingeralesy@gmail.com>
2018-04-24 14:42:17 +0300

    Fixed triangulation

--- physics/math.lisp
+++ physics/math.lisp
@@ -46,6 +46,36 @@ If C is provided, then A, B, and C are points where B->A and B->C are the vector
     (atan (vz (vc vector other))
           (v. vector other))))
 
+(defun perpendicular (vec-a vec-b)
+  ;; TODO: more than 2D?
+  (let ((x (- (vy vec-a) (vy vec-b)))
+        (y (- (vx vec-b) (vx vec-a))))
+    (nv* (vec2 x y) (/ (sqrt (+ (* x x) (* y y)))))))
+
+(defun intersect-circles (center-a range-a center-b range-b)
+  "Gets the intersections of two *circles* (not spheres)."
+  (declare (type vec2 center-a center-b))
+  (declare (type number range-a range-b))
+  (let ((dist (vdistance center-a center-b)))
+    (if (or (< (+ range-a range-b) dist)            ;; Do not overlap
+            (< dist (abs (- range-a range-b)))      ;; One contains the other
+            (and (= dist 0.0) (= range-a range-b))) ;; They are the same
+        (values NIL NIL) ;; We return NIL points
+        ;; Get distances of dimensions from the first point
+        (let* ((a (+ (- (* range-a range-a) (* range-b range-b))
+                     (/ (* dist dist) (* 2 dist))))
+               (h (sqrt (- (* range-a range-a) (* a a))))
+               ;; Determine point on the line between centers perpendicular to intersects
+               (diff (v- center-b center-a))
+               (perp (nv+ (v* diff (/ a dist)) center-a))
+               (h-by-dist (/ h dist)))
+          ;; And then the two intersections
+          (nv* diff h-by-dist)
+          (values (vec2 (round-to (+ (vx perp) (vy diff)) 4)
+                        (round-to (- (vy perp) (vx diff)) 4))
+                  (vec2 (round-to (- (vx perp) (vy diff)) 4)
+                        (round-to (+ (vy perp) (vx diff)) 4)))))))
+
 (defun triangulate (point-a dist-a point-b dist-b point-c dist-c)
   "Calculates the location in a 2D plane based on three other points and their distances from the location."
   (declare (type vec point-a point-b point-c))
@@ -53,25 +83,38 @@ If C is provided, then A, B, and C are points where B->A and B->C are the vector
   (when (v= point-a point-b) (error "POINT-A and POINT-B are equal"))
   (when (v= point-a point-c) (error "POINT-A and POINT-C are equal"))
   (when (v= point-b point-c) (error "POINT-B and POINT-C are equal"))
-  (let* ((vec-ab (v- point-a point-b))
-         (dist (vlength vec-ab))
-         (a (/ (+ (* dist-a dist-a) (- (* dist-b dist-b)) (* dist dist)) (* 2 dist)))
-         (h2 (- (* dist-a dist-a) (* a a)))
-         (h (* (sqrt (abs h2)) (if (< 0 h2) -1 1)))
-         (point-c (vec (vx point-c) (vy point-c)))
-         (point-mid (v+ point-a (v* vec-ab a (/ dist))))
-         (center-1 (vec2 (+ (vx point-mid)
-                            (* h (- (vy point-b) (vy point-a)) (/ dist)))
-                         (- (vy point-mid)
-                            (* h (- (vx point-b) (vx point-a)) (/ dist)))))
-         (center-2 (vec2 (- (vx point-mid)
-                            (* h (- (vy point-b) (vy point-a)) (/ dist)))
-                         (+ (vy point-mid)
-                            (* h (- (vx point-b) (vx point-a)) (/ dist)))))
-         (dist-1 (abs (vlength (v- point-c center-1))))
-         (dist-2 (abs (vlength (v- point-c center-2)))))
-    (if (< (abs (- (abs dist-c) dist-1)) (abs (- (abs dist-c) dist-2)))
-        center-1 center-2)))
+  (let* ((point-a (ensure-vector-type point-a 'vec2))
+         (point-b (ensure-vector-type point-b 'vec2))
+         (point-c (ensure-vector-type point-c 'vec2))
+         (circles (list (cons point-a dist-a)
+                        (cons point-b dist-b)
+                        (cons point-c dist-c)))
+         (points ())
+         (center-offset NIL)
+         (center NIL))
+    (for:for ((i repeat 3)
+              (circle-a = (nth (mod i 3) circles))
+              (circle-b = (nth (mod (1+ i) 3) circles))
+              (circle-c = (nth (mod (+ 2 i) 3) circles))
+              ((isect-a isect-b) = (multiple-value-list
+                                    (intersect-circles
+                                     (car circle-a) (cdr circle-a)
+                                     (car circle-b) (cdr circle-b)))))
+      (when (and isect-a isect-b)
+        (let* ((offset-a (- (vdistance isect-a (car circle-c)) (cdr circle-c)))
+               (offset-b (- (vdistance isect-b (car circle-c)) (cdr circle-c)))
+               (cross-points (list (cons isect-a offset-a)
+                                   (cons isect-b offset-b))))
+          (if points
+              (nconc points cross-points)
+              (setf points cross-points)))))
+    (for:for ((point in points)
+              (isect = (car point))
+              (offset = (cdr point)))
+      (when (or (null center) (< offset center-offset))
+        (setf center-offset offset
+              center isect)))
+    center))
 
 (defun ensure-vector-type (vector vector-type &optional (default (vec4 0 0 0 0)))
   (declare (type vec vector default))
@@ -93,7 +136,7 @@ If C is provided, then A, B, and C are points where B->A and B->C are the vector
 (defun round-to (number precision &optional (round-f #'round))
   (if (/= 0 number)
       (let ((div (expt 10 precision)))
-        (/ (funcall round-f (* number div)) div))
+        (coerce (/ (funcall round-f (* number div)) div) 'single-float))
       0.0))
 
 (defun square-distance (vec-a vec-b)
@@ -108,9 +151,3 @@ If C is provided, then A, B, and C are points where B->A and B->C are the vector
                (- (vw vec-a) (vw vec-b))
                0.0)))
     (+ (* x x) (* y y) (* z z) (* w w))))
-
-(defun perpendicular (vec-a vec-b)
-  ;; TODO: more than 2D?
-  (let ((x (- (vy vec-a) (vy vec-b)))
-        (y (- (vx vec-b) (vx vec-a))))
-    (nv* (vec2 x y) (/ (sqrt (+ (* x x) (* y y)))))))
