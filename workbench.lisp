60cde24496f7518267cc426009de7428b0bb1f1b
Shinmera <shinmera@tymoon.eu>
2018-06-17 14:12:04 +0200

    Start working on the update of the clipmaps, but for some reason it's being real fucking weird for reasons I cann't seem to figure out

--- workbench.lisp
+++ workbench.lisp
@@ -3,9 +3,42 @@
 (define-pool workbench
   :base 'trial)
 
+(define-asset (workbench cube) mesh
+    (make-cube 100))
+
+(define-asset (workbench cat) image
+    #p"cat.png")
+
+(define-shader-entity tester (vertex-entity textured-entity)
+  ()
+  (:default-initargs
+   :vertex-array (asset 'workbench 'cube))
+  (:inhibit-shaders (textured-entity :fragment-shader)))
+
+(define-class-shader (tester :fragment-shader)
+  "in vec2 texcoord;
+out vec4 color;
+uniform sampler2D texture_image;
+
+void main(){
+  color *= texture(texture_image, texcoord);
+}")
+
+(defmethod update :after ((main main) tt dt)
+  (show-region (unit :clipmap (scene main)) 0 0))
+
 (progn
   (defmethod setup-scene ((main main) scene)
-    (enter (make-instance 'target-camera :location (vec 70 -100 150)) scene)
+    (cond ((= 0 1)
+           (enter (make-instance '2d-camera) scene)
+           (enter (make-instance 'geometry-clipmap-updater) scene))
+          (T
+           (enter (make-instance 'editor-camera :move-speed 0.5
+                                                :rotation (VEC3 0.63999623 1.4305115e-6 0.0)
+                                                :location (VEC3 -0.79987687 497.58453 775.5049)) scene)
+           (let ((clipmap (make-instance 'geometry-clipmap :data-directory #p"~/clipmaps/" :name :clipmap)))
+             (enter clipmap scene)
+             (enter (make-instance 'tester :texture (texture-buffer clipmap)) scene))))
     (enter (make-instance 'render-pass) scene))
 
   (maybe-reload-scene))
