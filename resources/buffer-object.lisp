88d1a7914104e121e5f6047d366e637d4425a525
Shinmera <shinmera@tymoon.eu>
2018-06-29 13:40:47 +0200

    Make the semantics more sensible

--- resources/buffer-object.lisp
+++ resources/buffer-object.lisp
@@ -9,56 +9,60 @@
 (defclass buffer-object (gl-resource)
   ((buffer-type :initarg :buffer-type :accessor buffer-type)
    (buffer-data :initarg :buffer-data :accessor buffer-data)
-   (element-type :initarg :element-type :accessor element-type)
    (data-usage :initarg :data-usage :accessor data-usage)
    (size :initarg :size :initform NIL :accessor size))
   (:default-initargs
    :buffer-type (error "BUFFER-TYPE required.")
    :data-usage :static-draw
-   :buffer-data NIL
-   :element-type NIL))
+   :buffer-data NIL))
 
-(defmethod initialize-instance :before ((buffer buffer-object) &key buffer-type element-type data-usage)
+(defmethod initialize-instance :before ((buffer buffer-object) &key buffer-type data-usage)
   (check-buffer-object-type buffer-type)
-  (check-buffer-object-element-type element-type)
   (check-buffer-object-data-usage data-usage))
 
 (defmethod print-object ((buffer buffer-object) stream)
   (print-unreadable-object (buffer stream :type T :identity T)
-    (format stream "~a ~a ~a" (data-usage buffer) (buffer-type buffer) (element-type buffer))))
+    (format stream "~a ~a" (buffer-type buffer) (data-usage buffer))))
 
 (defmethod destructor ((buffer buffer-object))
   (let ((vbo (gl-name buffer)))
     (lambda () (when vbo (gl:delete-buffers (list vbo))))))
 
-(defgeneric update-buffer-data (buffer data &key data-start data-end buffer-start buffer-end))
+;; ELEMENT-TYPE is forced to (UNSIGNED-BYTE 8) for data pointers.
+;; DATA-START and DATA-END are in units of the size of ELEMENT-TYPE.
+;; BUFFER-START and BUFFER-END are always in (UNSIGNED-BYTE 8) units.
+(defgeneric update-buffer-data (buffer data &key data-start data-end buffer-start buffer-end element-type))
 
-(defmethod update-buffer-data ((buffer buffer-object) data &key data-start data-end buffer-start buffer-end)
+(defmethod update-buffer-data ((buffer buffer-object) data &key data-start data-end buffer-start buffer-end element-type)
+  (declare (ignore element-type))
   (etypecase data
     (cffi:foreign-pointer
-     (with-slots (element-type buffer-type data-usage size) buffer
+     (with-slots (buffer-type data-usage size) buffer
        (gl:bind-buffer buffer-type (gl-name buffer))
        (unwind-protect
             (let* ((pointer (if data-start
                                 (cffi:inc-pointer data data-start)
                                 data))
-                   (element-size (cffi:foreign-type-size element-type))
                    (buffer-start (or buffer-start 0)))
               (if (or buffer-start buffer-end)
-                  (%gl:buffer-sub-data buffer-type (or buffer-start 0) (- (or data-end buffer-end (* size element-size)) data-start) pointer)
-                  (%gl:buffer-data buffer-type (* size element-size) pointer data-usage)))
+                  (%gl:buffer-sub-data buffer-type (or buffer-start 0) (- (or data-end buffer-end size) data-start) pointer)
+                  (%gl:buffer-data buffer-type size pointer data-usage)))
          (gl:bind-buffer buffer-type 0))))))
 
-(defmethod update-buffer-data ((buffer buffer-object) (data null) &key data-start data-end buffer-start buffer-end)
-  (declare (ignore data-start data-end))
-  (update-buffer-data buffer (cffi:null-pointer) :buffer-start buffer-start :buffer-end buffer-end))
+(defmethod update-buffer-data ((buffer buffer-object) (data null) &rest args)
+  (apply #'update-buffer-data buffer (cffi:null-pointer) :data-start 0 args))
 
-(defmethod update-buffer-data ((buffer buffer-object) (data real) &key data-start data-end buffer-start buffer-end)
+(defmethod update-buffer-data ((buffer buffer-object) (data real) &key data-start data-end buffer-start buffer-end element-type)
   (declare (ignore data-start data-end buffer-end))
-  (cffi:with-foreign-object (data (element-type buffer) 1)
-    (let ((buffer-start (or buffer-start 0))
-          (buffer-end (+ buffer-start (cffi:foreign-type-size (element-type buffer)))))
-      (setf (cffi:mem-ref data (element-type buffer)) (gl-coerce data (element-type buffer)))
+  (let ((element-type (or element-type
+                          (etypecase data
+                            (single-float :float)
+                            (double-float :double)
+                            (T :integer))))
+        (buffer-start (or buffer-start 0))
+        (buffer-end (+ buffer-start (cffi:foreign-type-size (element-type buffer)))))
+    (cffi:with-foreign-object (data element-type 1)
+      (setf (cffi:mem-ref data element-type) (gl-coerce data element-type))
       (update-buffer-data buffer data :buffer-start buffer-start :buffer-end buffer-end))))
 
 (defmethod update-buffer-data ((buffer buffer-object) (buffer-data mat2) &rest args)
@@ -102,9 +106,9 @@
       (setf (aref arr 3) (vw4 data))
       (apply #'update-buffer-data buffer arr args))))
 
-(defmethod update-buffer-data ((buffer buffer-object) (buffer-data vector) &key (data-start 0) (data-end (length buffer-data)) buffer-start buffer-end)
-  (let* ((element-type (element-type buffer))
-         (element-size (cffi:foreign-type-size (element-type buffer)))
+(defmethod update-buffer-data ((buffer buffer-object) (buffer-data vector) &key (data-start 0) (data-end (length buffer-data)) buffer-start buffer-end element-type)
+  (let* ((element-type (or element-type (array-element-type buffer-data)))
+         (element-size (cffi:foreign-type-size element-type))
          (buffer-start (when buffer-start (* buffer-start element-size)))
          (buffer-end (when buffer-end (* buffer-end element-size))))
     (cond ((static-vector-p buffer-data)
--- resources/vertex-buffer.lisp
+++ resources/vertex-buffer.lisp
@@ -7,7 +7,14 @@
 (in-package #:org.shirakumo.fraf.trial)
 
 (defclass vertex-buffer (buffer-object)
-  ()
+  ((element-type :initarg :element-type :accessor element-type))
   (:default-initargs
    :buffer-type :array-buffer
    :element-type :float))
+
+(defmethod update-buffer-data ((buffer vertex-buffer) data &rest args)
+  (let ((element-type (getf args :element-type)))
+    (when (and element-type (not (equal element-type (element-type buffer))))
+      (error "Cannot update vertex buffer of element type ~s with type ~s."
+             (element-type buffer) element-type)))
+  (apply #'call-next-method buffer data :element-type (element-type buffer) args))
