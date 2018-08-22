caf9cb13eb1dc57b6b2c5922866885038757b779
Shinmera <shinmera@tymoon.eu>
2018-08-22 22:52:49 +0200

    Make mmap stuff OS-conditionalised.

--- mmap.lisp
+++ mmap.lisp
@@ -51,6 +51,7 @@
   (declare (type fixnum open-flags protection-flags map-flags))
   (declare (type string path))
   (declare (optimize speed))
+  #+unix
   (let ((fd (osicat-posix:open path open-flags)))
     (unwind-protect
          (let* ((size (osicat-posix:stat-size (osicat-posix:fstat fd)))
@@ -59,7 +60,12 @@
                                          map-flags
                                          fd 0)))
            (values addr size))
-      (osicat-posix:close fd))))
+      (osicat-posix:close fd)))
+  ;; FIXME: This https://msdn.microsoft.com/en-us/library/aa366761.aspx
+  #+windows
+  (error "Implement MMAP compatibility layer on Windows you fuck")
+  #-(or windows unix)
+  (error "What kind of system even is this?"))
 
 (defun mmap (path &key (open '(:read)) (protection '(:read)) (mmap '(:private)))
   (%mmap (etypecase path
@@ -83,7 +89,12 @@
 
 (declaim (inline munmap))
 (defun munmap (addr size)
-  (osicat-posix:munmap addr size))
+  #+unix
+  (osicat-posix:munmap addr size)
+  #+windows
+  (error "Implement MMAP compatibility layer on Windows you fuck")
+  #-(or windows unix)
+  (error "What kind of system even is this?"))
 
 (defmacro with-mmap ((addr size path &rest args) &body body)
   `(multiple-value-bind (,addr ,size) (mmap ,path ,@args)
