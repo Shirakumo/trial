9218f9e8864af74e7175e3d104d4923024f4b5e5
Shinmera <shinmera@tymoon.eu>
2019-01-28 19:19:39 +0100

    Minor

--- shadow-map.lisp
+++ shadow-map.lisp
@@ -9,7 +9,10 @@
 (define-shader-pass shadow-map-pass (per-object-pass)
   ((shadow :port-type output
            :attachment :depth-attachment
-           :texspec (:width 1024 :height 1024 :wrapping :clamp-to-border)))
+           :texspec (:width 1024
+                     :height 1024
+                     :wrapping :clamp-to-border
+                     :border-color #.(vec 1 1 1 1))))
   (:inhibit-shaders (shader-entity :fragment-shader)))
 
 (define-class-shader (shadow-map-pass :fragment-shader)
@@ -38,11 +41,23 @@ void main(){}")
 uniform sampler2D shadow_map;
 uniform mat4 light_space_matrix;
 
-vec2 poissonDisk[4] = vec2[](
-  vec2( -0.94201624, -0.39906216 ),
-  vec2( 0.94558609, -0.76890725 ),
-  vec2( -0.094184101, -0.92938870 ),
-  vec2( 0.34495938, 0.29387760 )
+vec2 poisson_disk[16] = vec2[](
+   vec2( -0.94201624, -0.39906216 ),
+   vec2( 0.94558609, -0.76890725 ),
+   vec2( -0.094184101, -0.92938870 ),
+   vec2( 0.34495938, 0.29387760 ),
+   vec2( -0.91588581, 0.45771432 ),
+   vec2( -0.81544232, -0.87912464 ),
+   vec2( -0.38277543, 0.27676845 ),
+   vec2( 0.97484398, 0.75648379 ),
+   vec2( 0.44323325, -0.97511554 ),
+   vec2( 0.53742981, -0.47373420 ),
+   vec2( -0.26496911, -0.41893023 ),
+   vec2( 0.79197514, 0.19090188 ),
+   vec2( -0.24188840, 0.99706507 ),
+   vec2( -0.81409955, 0.91437590 ),
+   vec2( 0.19984126, 0.78641367 ),
+   vec2( 0.14383161, -0.14100790 )
 );
 
 float random(vec4 seed4){
@@ -61,7 +76,7 @@ float shadow_factor(vec3 position, float bias){
   vec2 texel_size = 1.0 / textureSize(shadow_map, 0);
   for(int i=0; i<4; ++i){
     int index = int(16.0*random(vec4(gl_FragCoord.xyy, i)))%16;
-    vec2 poisson = poissonDisk[index]/1000.0;
+    vec2 poisson = poisson_disk[index]/1000.0;
     for(int x=-1; x<=1; ++x){
       for(int y=-1; y<=1; ++y){
         float closest = texture(shadow_map, projected.xy + vec2(x, y)*texel_size + poisson).r;
