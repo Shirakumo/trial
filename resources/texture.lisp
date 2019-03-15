7173384371e41a71ec6bfd8ae5f9a33d3c11bd6e
Shinmera <shinmera@tymoon.eu>
2019-03-15 15:24:26 +0100

    Forgot a debug statement

--- resources/texture.lisp
+++ resources/texture.lisp
@@ -114,7 +114,6 @@
 (defun allocate-texture-storage (texture)
   (with-accessors* (target storage levels internal-format width height depth
                            samples pixel-format pixel-type pixel-data) texture
-    (v:info :FUCK "~a " (list internal-format width height pixel-format pixel-type))
     (let ((internal-format (cffi:foreign-enum-value '%gl:enum internal-format)))
       ;; FIXME: Handle array cases better, factor this out into an update routine.
       (case target
