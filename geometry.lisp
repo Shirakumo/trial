0457859155a9ffad6d5563c7d23da611a63503f9
Shinmera <shinmera@tymoon.eu>
2020-06-05 14:37:14 +0200

    Remove the change-class hack.

--- geometry.lisp
+++ geometry.lisp
@@ -241,37 +241,3 @@
   ;; Would be better if we didn't have to create an adjustable vector...
   (replace-vertex-data (make-array 0 :adjustable T :element-type 'single-float)
                        mesh :attributes attributes))
-
-(defmethod update-instance-for-different-class ((mesh vertex-mesh) (array vertex-array) &key (data-usage :static-draw) (vertex-attributes T) vertex-form)
-  (setf (vertex-form array)
-        (or vertex-form
-            (ecase (face-length mesh)
-              (1 :points)
-              (2 :lines)
-              (3 :triangles))))
-  (let* ((primer (if (= 0 (length (vertices mesh)))
-                     (allocate-instance (find-class (vertex-type mesh)))
-                     (aref (vertices mesh) 0)))
-         (attributes (etypecase vertex-attributes
-                       ((eql T) (vertex-attributes primer))
-                       (list vertex-attributes)))
-         (sizes (loop for attr in attributes collect (vertex-attribute-size primer attr)))
-         (buffer (make-vertex-data mesh :attributes attributes)))
-    (setf (data-pointer array) NIL)
-    ;; Construct the buffers and specs
-    (let* ((vbo (make-instance 'vertex-buffer :buffer-data buffer :buffer-type :array-buffer
-                                              :data-usage data-usage :element-type :float
-                                              :size (* (length buffer) (gl-type-size :float))))
-           (ebo (make-instance 'vertex-buffer :buffer-data (faces mesh) :buffer-type :element-array-buffer
-                                              :data-usage data-usage :element-type :unsigned-int
-                                              :size (* (length (faces mesh)) (gl-type-size :unsigned-int))))
-           (specs (loop with stride = (reduce #'+ sizes)
-                        for offset = 0 then (+ offset size)
-                        for size in sizes
-                        for index from 0
-                        collect (list vbo :stride (* stride (gl-type-size :float))
-                                          :offset (* offset (gl-type-size :float))
-                                          :size size
-                                          :index index))))
-      (setf (bindings array) (list* ebo specs))
-      (setf (size array) (length (faces mesh))))))
