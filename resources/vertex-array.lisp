425f518a8f8cf10b06b6c373cf6dde9d216b3b76
Shinmera <shinmera@tymoon.eu>
2020-06-05 13:14:01 +0200

    Similar to shader-program, hot-patch bindings in a vertex-array if already loaded.

--- resources/vertex-array.lisp
+++ resources/vertex-array.lisp
@@ -25,37 +25,45 @@
 (defmethod dependencies ((array vertex-array))
   (mapcar #'unlist (bindings array)))
 
+(defun update-array-bindings (array bindings)
+  (gl:bind-vertex-array (data-pointer array))
+  (unwind-protect
+       (loop for binding in bindings
+             for i from 0
+             do (destructuring-bind (buffer &key (index i)
+                                                 (size 3)
+                                                 (stride 0)
+                                                 (offset 0)
+                                                 (normalize NIL)
+                                                 (instancing 0)
+                                                 (type))
+                    (enlist binding)
+                  (check-allocated buffer)
+                  (gl:bind-buffer (buffer-type buffer) (gl-name buffer))
+                  (ecase (buffer-type buffer)
+                    (:element-array-buffer
+                     (unless (size array)
+                       (setf (size array) (/ (size buffer) (gl-type-size (element-type buffer)))))
+                     (decf i))
+                    (:array-buffer
+                     (ecase (or type (element-type buffer))
+                       ((:half-float :float :fixed)
+                        (gl:vertex-attrib-pointer index size (element-type buffer) normalize stride offset))
+                       ((:byte :unsigned-byte :short :unsigned-short :int :unsigned-int)
+                        (gl:vertex-attrib-ipointer index size (element-type buffer) stride offset))
+                       (:double
+                        (%gl:vertex-attrib-lpointer index size (element-type buffer) stride offset)))
+                     (gl:enable-vertex-attrib-array index)
+                     (%gl:vertex-attrib-divisor index instancing)))))
+    (gl:bind-vertex-array 0)))
+
+(defmethod (setf bindings) :after (bindings (array vertex-array))
+  (when (allocated-p array)
+    (update-array-bindings arary bindings)))
+
 (defmethod allocate ((array vertex-array))
   (let ((vao (gl:gen-vertex-array)))
-    (with-cleanup-on-failure (gl:delete-vertex-arrays (list vao))
-      (gl:bind-vertex-array vao)
-      (unwind-protect
-           (loop for binding in (bindings array)
-                 for i from 0
-                 do (destructuring-bind (buffer &key (index i)
-                                                     (size 3)
-                                                     (stride 0)
-                                                     (offset 0)
-                                                     (normalize NIL)
-                                                     (instancing 0)
-                                                     (type))
-                        (enlist binding)
-                      (check-allocated buffer)
-                      (gl:bind-buffer (buffer-type buffer) (gl-name buffer))
-                      (ecase (buffer-type buffer)
-                        (:element-array-buffer
-                         (unless (size array)
-                           (setf (size array) (/ (size buffer) (gl-type-size (element-type buffer)))))
-                         (decf i))
-                        (:array-buffer
-                         (ecase (or type (element-type buffer))
-                           ((:half-float :float :fixed)
-                            (gl:vertex-attrib-pointer index size (element-type buffer) normalize stride offset))
-                           ((:byte :unsigned-byte :short :unsigned-short :int :unsigned-int)
-                            (gl:vertex-attrib-ipointer index size (element-type buffer) stride offset))
-                           (:double
-                            (%gl:vertex-attrib-lpointer index size (element-type buffer) stride offset)))
-                         (gl:enable-vertex-attrib-array index)
-                         (%gl:vertex-attrib-divisor index instancing)))))
-        (gl:bind-vertex-array 0)
-        (setf (data-pointer array) vao)))))
+    (with-cleanup-on-failure (progn (gl:delete-vertex-arrays (list vao))
+                                    (setf (data-pointer array) NIL))
+      (setf (data-pointer array) vao)
+      (update-array-bindings array (bindings array)))))
