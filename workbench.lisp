c33a912efef4e005d31a00b6cf1eda65b97672aa
Shinmera <shinmera@tymoon.eu>
2021-03-22 19:10:47 +0100

    Push a more minimal, more useful workbench.

--- workbench.lisp
+++ workbench.lisp
@@ -1,9 +1,7 @@
 (defpackage #:workbench
   (:use #:cl+trial)
   (:shadow #:launch)
-  (:local-nicknames
-   (#:harmony #:org.shirakumo.fraf.harmony.user)
-   (#:mixed #:org.shirakumo.fraf.mixed))
+  (:local-nicknames)
   (:export #:workbench #:launch))
 (in-package #:workbench)
 
@@ -15,10 +13,35 @@
 
 (define-pool workbench)
 
+(define-asset (workbench cat) image
+    #p"cat.png")
+
+(define-asset (workbench cube) mesh
+    (make-cube 15))
+
+(define-asset (workbench grid) mesh
+    (make-line-grid 10 100 100))
+
+(define-shader-entity player (vertex-entity textured-entity located-entity listener)
+  ((name :initform 'player)
+   (texture :initform (// 'workbench 'cat))
+   (vertex-array :initform (// 'workbench 'cube))))
+
+(define-handler (player tick) (dt)
+  (when (retained :w)
+    (incf (vz (location player)) (* dt +50)))
+  (when (retained :a)
+    (incf (vx (location player)) (* dt +50)))
+  (when (retained :s)
+    (incf (vz (location player)) (* dt -50)))
+  (when (retained :d)
+    (incf (vx (location player)) (* dt -50))))
+
 (progn
   (defmethod setup-scene ((workbench workbench) scene)
-    (disable :cull-face)
-    (disable :depth-test)
     (enter (make-instance 'trial::fps-counter) scene)
+    (enter (make-instance 'vertex-entity :vertex-array (// 'workbench 'grid)) scene)
+    (enter (make-instance 'player) scene)
+    (enter (make-instance 'following-camera :target (unit 'player scene) :location (vec 0 100 -100)) scene)
     (enter (make-instance 'render-pass) scene))
   (maybe-reload-scene))
