dc542583965d8feee0683db8ed167af12775c3a4
Shinmera <shinmera@tymoon.eu>
2021-03-29 18:01:41 +0200

    Minor

--- event-loop.lisp
+++ event-loop.lisp
@@ -57,15 +57,18 @@
 ;;        The check will assume nothing has changed and it'll continue from
 ;;        where it left off, thus missing events before the current index.
 (defmethod process ((loop event-loop))
+  (declare (optimize speed))
   (with-simple-restart (discard-events "Discard all events.")
-    (loop for i = (1- (incf (queue-index loop)))
-          while (< i (length (queue loop)))
-          do (let ((event (aref (queue loop) i)))
-               (when event
-                 (handle event loop)
-                 (setf (aref (queue loop) i) NIL)))))
-  (setf (fill-pointer (queue loop)) 0
-        (queue-index loop) 0))
+    (let ((queue (queue loop)))
+      (declare (type (and (vector T) (not simple-array)) queue))
+      (loop for i = (1- (the (unsigned-byte 32) (incf (queue-index loop))))
+            while (< i (length queue))
+            do (let ((event (aref queue i)))
+                 (when event
+                   (handle event loop)
+                   (setf (aref queue i) NIL))))
+      (setf (fill-pointer queue) 0
+            (queue-index loop) 0))))
 
 (defun discard-events (loop)
   (loop for i = (1- (incf (queue-index loop)))
