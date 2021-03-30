19ddda13f659d28ec570d665543c860dda1e7f4c
Shinmera <shinmera@tymoon.eu>
2021-03-30 16:21:43 +0200

    Minor

--- gamepad.lisp
+++ gamepad.lisp
@@ -9,19 +9,19 @@
 (defclass gamepad-input-handler ()
   ((last-device-probe :initform 0 :accessor last-device-probe)))
 
-(defmacro with-gamepad-failure-handling ((&key (restart ''ignore-gamepad-failure)) &body body)
-  `(with-simple-restart (ignore-gamepad-failure "Ignore the gamepad failure.")
+(defmacro with-gamepad-failure-handling ((&key (ignore-error T)) &body body)
+  `(catch 'bail-input
      (handler-bind ((gamepad:gamepad-error
                       (lambda (e)
                         (declare (ignore e))
                         (when (find-restart 'gamepad:drop-device)
                           (invoke-restart 'gamepad:drop-device))
-                        ,(when restart
-                           `(invoke-restart ,restart)))))
+                        ,(when ignore-error
+                           `(throw 'bail-input NIL)))))
        ,@body)))
 
 (defmethod start :after ((handler gamepad-input-handler))
-  (with-gamepad-failure-handling (:restart NIL)
+  (with-gamepad-failure-handling (:ignore-error NIL)
     (gamepad:init)))
 
 (defmethod stop :after ((handler gamepad-input-handler))
@@ -30,30 +30,31 @@
 
 (defmethod poll-input :after ((handler gamepad-input-handler))
   (with-gamepad-failure-handling ()
-    (flet ((process (event)
-             (typecase event
-               (gamepad:button-down
-                (handle (make-instance 'gamepad-press
-                                       :button (or (gamepad:event-label event)
+    (labels ((process (event)
+               (typecase event
+                 (gamepad:button-down
+                  (handle (make-instance 'gamepad-press
+                                         :button (or (gamepad:event-label event)
+                                                     (gamepad:event-code event))
+                                         :device (gamepad:event-device event))
+                          handler))
+                 (gamepad:button-up
+                  (handle (make-instance 'gamepad-release
+                                         :button (or (gamepad:event-label event)
+                                                     (gamepad:event-code event))
+                                         :device (gamepad:event-device event))
+                          handler))
+                 (gamepad:axis-move
+                  (handle (make-instance 'gamepad-move
+                                         :pos (gamepad:event-value event)
+                                         :old-pos (gamepad:event-old-value event)
+                                         :axis (or (gamepad:event-label event)
                                                    (gamepad:event-code event))
-                                       :device (gamepad:event-device event))
-                        handler))
-               (gamepad:button-up
-                (handle (make-instance 'gamepad-release
-                                       :button (or (gamepad:event-label event)
-                                                   (gamepad:event-code event))
-                                       :device (gamepad:event-device event))
-                        handler))
-               (gamepad:axis-move
-                (handle (make-instance 'gamepad-move
-                                       :pos (gamepad:event-value event)
-                                       :old-pos (gamepad:event-old-value event)
-                                       :axis (or (gamepad:event-label event)
-                                                 (gamepad:event-code event))
-                                       :device (gamepad:event-device event))
-                        handler)))))
-      (dolist (device (gamepad:list-devices))
-        (gamepad:poll-events device #'process)))
+                                         :device (gamepad:event-device event))
+                          handler))))
+             (poll (device)
+               (gamepad:poll-events device #'process)))
+      (gamepad:call-with-devices #'poll))
     (when (< internal-time-units-per-second
              (- (get-internal-real-time) (last-device-probe handler)))
       (setf (last-device-probe handler) (get-internal-real-time))
