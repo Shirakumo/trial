df2c85c0caee35ce98e3bd749b712b25b49bbc33
Shinmera <shinmera@tymoon.eu>
2021-04-08 10:57:53 +0200

    remove obsolete nick

--- backends/glfw/package.lisp
+++ backends/glfw/package.lisp
@@ -5,8 +5,7 @@
 |#
 
 (in-package #:cl-user)
-(defpackage #:trial-glfw
-  (:nicknames #:org.shirakumo.fraf.trial.glfw)
+(defpackage #:org.shirakumo.fraf.trial.glfw
   (:use #:cl+trial #:3d-vectors)
   (:shadow #:context #:window)
   (:shadowing-import-from #:trial #:load)
