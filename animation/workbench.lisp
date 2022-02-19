2ef7928a5382a2c93b01a3edb84fdc08f43c2ff6
Shinmera <shinmera@tymoon.eu>
2022-02-19 21:46:11 +0100

    Minor

--- animation/workbench.lisp
+++ animation/workbench.lisp
@@ -12,7 +12,8 @@
 (trial:define-asset (workbench simple) gltf-asset
   ;; #p "~/Projects/var/gltf-samples/2.0/Box/glTF/Box.gltf"
   ;; #p "~/Projects/var/gltf-samples/2.0/BoxTextured/glTF/BoxTextured.gltf"
-  #p "~/Projects/var/gltf-samples/2.0/SimpleSkin/glTF/SimpleSkin.gltf"
+    ;; #p "~/Projects/var/gltf-samples/2.0/SimpleSkin/glTF/SimpleSkin.gltf"
+  #p "~/Projects/var/gltf-samples/2.0/RiggedSimple/glTF/RiggedSimple.gltf"
   )
 
 (trial:define-asset (workbench grid) trial:mesh
@@ -23,7 +24,8 @@
     (trial:disable :cull-face)
     (trial:enter (make-instance 'trial::fps-counter) scene)
     (trial:enter (make-instance 'trial:vertex-entity :vertex-array (trial:// 'workbench 'grid)) scene)
-    (trial:enter (make-instance 'entity :asset (trial:asset 'workbench 'simple) :mesh 0) scene)
+    (trial:enter (make-instance 'entity :asset (trial:asset 'workbench 'simple) :mesh "Cylinder") scene)
+    ;;(trial:enter (make-instance 'trial:vertex-entity :vertex-array (trial:// 'workbench 'simple "Cylinder")) scene)
     (trial:enter (make-instance 'trial:editor-camera :location (vec 0 1 5)) scene)
     (trial:enter (make-instance 'trial:render-pass) scene))
   (trial:maybe-reload-scene))
