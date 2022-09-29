c139bc80c1e3dd8a004085cc17ad40077b4c0f93
Shinmera <shinmera@tymoon.eu>
2022-09-29 20:49:05 +0200

    Whoops.

---
+++ alloy/actions.lisp
@@ -0,0 +1,50 @@
+#|
+ This file is a part of trial
+ (c) 2019 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.shirakumo.fraf.trial.alloy)
+
+(trial:define-action-set ui-actions)
+(trial:define-action select-left (ui-actions))
+(trial:define-action select-right (ui-actions))
+(trial:define-action select-up (ui-actions))
+(trial:define-action select-down (ui-actions))
+(trial:define-action accept (ui-actions))
+(trial:define-action back (ui-actions))
+
+(defmethod trial:handle ((ev select-left) (ui ui))
+  (alloy:handle (load-time-value (make-instance 'alloy:focus-left)) ui))
+
+(defmethod trial:handle ((ev select-right) (ui ui))
+  (alloy:handle (load-time-value (make-instance 'alloy:focus-right)) ui))
+
+(defmethod trial:handle ((ev select-up) (ui ui))
+  (alloy:handle (load-time-value (make-instance 'alloy:focus-up)) ui))
+
+(defmethod trial:handle ((ev select-down) (ui ui))
+  (alloy:handle (load-time-value (make-instance 'alloy:focus-down)) ui))
+
+(defmethod trial:handle ((ev accept) (ui ui))
+  (alloy:handle (load-time-value (make-instance 'alloy:activate)) ui))
+
+(defmethod trial:handle ((ev back) (ui ui))
+  (alloy:handle (load-time-value (make-instance 'alloy:exit)) ui))
+
+(defmethod trial:handle :after ((ev trial:tick) (ui ui))
+  (let ((hold (first-hold-time ui)))
+    (loop for action in '(select-left select-right select-up select-down)
+          do (when (trial:retained action)
+               (if (eql action (car hold))
+                   (incf (cdr hold) (trial:dt ev))
+                   (setf (car hold) action
+                         (cdr hold) 0.0))
+               (return))
+          finally (setf (car hold) NIL
+                        (cdr hold) 0.0))
+    ;; 0.5 is the initial repeat delay
+    (when (< 0.5 (cdr hold))
+      (trial:handle trial:+main+ (trial:make-event (car hold)))
+      ;; 0.1 is the delay between repeats
+      (setf (cdr hold) (- 0.5 0.1)))))
