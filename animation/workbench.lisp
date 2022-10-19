e5a80694bd1f7b4273ff57857fbfcf003f51535d
Shinmera <shinmera@tymoon.eu>
2022-10-19 14:20:14 +0200

    Sigh

--- animation/workbench.lisp
+++ animation/workbench.lisp
@@ -10,12 +10,12 @@
 (trial:define-pool workbench)
 
 (trial:define-asset (workbench simple) gltf-asset
-  ;; #p "~/Projects/var/gltf-samples/2.0/Box/glTF/Box.gltf"
-  ;; #p "~/Projects/var/gltf-samples/2.0/BoxTextured/glTF/BoxTextured.gltf"
-  ;; #p "~/Projects/var/gltf-samples/2.0/SimpleSkin/glTF/SimpleSkin.gltf"
+    ;; #p "~/Projects/var/gltf-samples/2.0/Box/glTF/Box.gltf"
+    ;; #p "~/Projects/var/gltf-samples/2.0/BoxTextured/glTF/BoxTextured.gltf"
+    ;; #p "~/Projects/var/gltf-samples/2.0/SimpleSkin/glTF/SimpleSkin.gltf"
     ;; #p "~/Projects/var/gltf-samples/2.0/RiggedSimple/glTF/RiggedSimple.gltf"
-  #p "~/Projects/var/gltf-samples/2.0/RiggedFigure/glTF/RiggedFigure.gltf"
-  )
+    ;; #p "~/Projects/var/gltf-samples/2.0/RiggedFigure/glTF/RiggedFigure.gltf"
+    #p "~/Projects/cl/trial-assets/data/Woman.gltf")
 
 (trial:define-asset (workbench grid) trial:mesh
     (trial:make-line-grid 10 10 10))
@@ -25,11 +25,8 @@
     (trial:disable :cull-face)
     (trial:enter (make-instance 'trial::fps-counter) scene)
     (trial:enter (make-instance 'trial:vertex-entity :vertex-array (trial:// 'workbench 'grid)) scene)
-    (let ((entity
-            (trial:enter (make-instance 'entity :name 'entity :asset (trial:asset 'workbench 'simple)) scene)
-            ;;(trial:enter (make-instance 'trial:vertex-entity :vertex-array (trial:// 'workbench 'simple "LOD3spShape")) scene)
-            ))
-      (setf (vy (trial:scaling entity)) -1.0)
-      (trial:enter (make-instance 'trial:target-camera :target (vec 0 1 0) :location (vec 0 2 -3)) scene))
+    ;(trial:enter (make-instance 'entity :name 'entity :asset (trial:asset 'workbench 'simple)) scene)
+    (trial:enter (make-instance 'lines :name 'entity :asset (trial:asset 'workbench 'simple)) scene)
+    (trial:enter (make-instance 'trial:target-camera :target (vec 0 1 0) :location (vec 0 2 -3)) scene)
     (trial:enter (make-instance 'trial:render-pass) scene))
   (trial:maybe-reload-scene))
