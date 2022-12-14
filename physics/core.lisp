2c22e092c60a41e2f85887d4723c201b923d45f9
Shinmera <shinmera@tymoon.eu>
2022-12-14 21:45:43 +0100

    Buncha stuff, start of a rigidbody engine.

---
+++ physics/core.lisp
@@ -0,0 +1,88 @@
+(in-package #:org.shirakumo.fraf.trial)
+
+(defgeneric integrate (object dt))
+(defgeneric mass (object))
+(defgeneric (setf mass) (object))
+
+(defclass physics-entity ()
+  ((velocity :initform (vec 0 0 0) :reader velocity)
+   (inverse-mass :initform 0.0 :accessor inverse-mass)
+   (force :initform (vec 0 0 0) :reader force)
+   (damping :initform 1.0 :accessor damping)))
+
+(defmethod shared-initialize :after ((entity physics-entity) slots &key mass)
+  (when mass (setf (mass entity) mass)))
+
+(defmethod (setf velocity) ((vel vec3) (entity physics-entity))
+  (v<- (velocity entity) vel))
+
+(defmethod (setf force) ((val vec3) (entity physics-entity))
+  (v<- (force entity) val))
+
+(defmethod mass ((entity physics-entity))
+  (let ((inverse (inverse-mass entity)))
+    (if (= 0.0 inverse)
+        0.0
+        (/ (inverse-mass entity)))))
+
+(defmethod (setf mass) (mass (entity physics-entity))
+  (setf (inverse-mass entity) (if (= 0.0 mass) 0.0 (abs (float (/ mass) 0.0)))))
+
+(defclass force ()
+  ())
+
+(defgeneric apply-force (force particle dt))
+
+(defmethod apply-force :around ((force force) (entity physics-entity) dt)
+  (when (< 0 (inverse-mass entity))
+    (call-next-method)))
+
+(defclass gravity (force)
+  ((gravity :initarg :gravity :initform (vec 0 -1 0) :accessor gravity)))
+
+(defmethod apply-force ((force gravity) (entity physics-entity) dt)
+  (nv+* (force entity) (gravity force) (mass entity)))
+
+(defclass drag-force (force)
+  ((k1 :initarg :k1 :initform 1.0 :accessor k1)
+   (k2 :initarg :k2 :initform 1.0 :accessor k2)))
+
+(defclass spring-force (force)
+  ((anchor :initarg :anchor :accessor anchor)
+   (anchor-offset :initarg :anchor-offset :initform (vec 0 0 0) :accessor anchor-offset)
+   (local-offset :initarg :local-offset :initform (vec 0 0 0) :accessor local-offset)
+   (spring-constant :initarg :spring-constant :initform 1.0 :accessor spring-constant)
+   (rest-length :initarg :rest-length :initform 0.0 :accessor rest-length)))
+
+(defclass stiff-spring-force (force)
+  ((anchor :initarg :anchor :accessor anchor)
+   (anchor-offset :initarg :anchor-offset :initform (vec 0 0 0) :accessor anchor-offset)
+   (local-offset :initarg :local-offset :initform (vec 0 0 0) :accessor local-offset)
+   (spring-constant :initarg :spring-constant :initform 1.0 :accessor spring-constant)
+   (damping :initarg :damping :initform 1.0 :accessor damping)))
+
+(defclass bungee-force (spring-force)
+  ())
+
+(defstruct hit
+  (a NIL :type T)
+  (b NIL :type T)
+  (restitution 0.0 :type single-float)
+  (normal (vec 0 0 0) :type vec3)
+  (depth 0.0 :type single-float))
+
+(defclass hit-generator () ())
+
+(defgeneric generate-hits (generator hits start end))
+
+(defmacro define-hit-generation (generator &body body)
+  `(defmethod generate-hits (,(enlist generator generator) hits start end)
+     (let ((hit (aref hits start)))
+       (block NIL
+         (flet ((finish-hit ()
+                  (incf start)
+                  (if (< start end)
+                      (setf hit (aref hits start))
+                      (return))))
+           ,@body))
+       start)))
---
+++ physics/rigidbody.lisp
@@ -0,0 +1,55 @@
+(in-package #:org.shirakumo.fraf.trial)
+
+(defclass rigidbody (physics-entity transformed-entity)
+  ((rotation :initform (vec 0 0 0) :reader rotation)
+   (inverse-inertia-tensor :initform (mat3) :reader inverse-inertia-tensor)
+   (torque :initform (vec 0 0 0) :reader torque)
+   (angular-damping :initform 1.0 :accessor angular-damping)
+   ;; Cache
+   (transform-matrix :initform (mat4) :reader transform-matrix)
+   (world-inverse-inertia-tensor :initform (mat3) :reader world-inverse-inertia-tensor)))
+
+(defmethod (setf rotation) ((vel vec3) (entity rigidbody))
+  (v<- (rotation entity) vel))
+
+(defmethod inertia-tensor ((entity rigidbody))
+  (minv (inverse-inertia-tensor entity)))
+
+(defmethod (setf inertia-tensor) ((mat mat3) (entity rigidbody))
+  (let ((inv (minv mat)))
+    (replace (marr3 (inverse-inertia-tensor entity)) (marr3 inv))))
+
+(defun %update-rigidbody-cache (rigidbody)
+  (nqunit (orientation rigidbody))
+  (tmat4 (tf rigidbody) (transform-matrix rigidbody))
+  (compute-world-inertia-tensor (world-inverse-inertia-tensor rigidbody) (inverse-inertia-tensor rigidbody) (transform-matrix rigidbody)))
+
+(defmethod impact-local ((entity rigidbody) force point)
+  ;; NOTE: The FORCE direction is in world coordinates, and the POINT is in local coordinates
+  (impact entity force (t*v (tf entity) point)))
+
+(defmethod impact ((entity rigidbody) force point)
+  (let ((local (v- point (location entity))))
+    (nv+ (force entity) force)
+    (nv+ (torque entity) (vc local force))))
+
+(defmethod apply-force ((force spring-force) (entity rigidbody) dt)
+  (let* ((lws (t*v (tf entity) (local-offset force)))
+         (ows (t*v (tf (anchor force)) (anchor-offset force)))
+         (force (v- lws ows))
+         (magnitude (* (spring-constant force) (abs (- (vlength force) (rest-length force))))))
+    (impact entity (nv* (nvunit force) (- magnitude)) lws)))
+
+(defmethod integrate ((entity rigidbody) dt)
+  (let ((last-frame-acceleration (nv* (force entity) (inverse-mass entity)))
+        (angular-acceleration (m* (world-inverse-inertia-tensor entity) (torque entity))))
+    (nv+* (velocity entity) last-frame-acceleration dt)
+    (nv+* (rotation entity) angular-acceleration dt)
+    (nv* (velocity entity) (expt (damping entity) dt))
+    (nv* (rotation entity) (expt (angular-damping entity) dt))
+    (nv+* (location entity) (velocity entity) dt)
+    (nv+* (orientation entity) (rotation entity) dt)
+
+    (%update-rigidbody-cache entity)
+    (vsetf (torque entity) 0 0 0)
+    (vsetf (force entity) 0 0 0)))
