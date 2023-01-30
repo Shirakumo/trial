6294ec1a21803c46062623bb7798ccdab14779bf
Shinmera <shinmera@tymoon.eu>
2023-01-30 11:51:03 +0100

    Ignore oom errors

--- harmony/deploy.lisp
+++ harmony/deploy.lisp
@@ -6,6 +6,13 @@
 
 (in-package #:org.shirakumo.fraf.trial.harmony)
 
+(defmethod trial:report-on-error ((error mixed:mixed-error))
+  (case (mixed:error-code error)
+    ((:out-of-memory 1)
+     (trial:emessage "Failed to allocate memory. Are you sure you have enough RAM free to run this game?"))
+    (T
+     (call-next-method))))
+
 #+linux
 (trial::dont-deploy
  org.shirakumo.fraf.mixed.pulse.cffi::libpulse-simple
