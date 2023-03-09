6f88fcbf5c6e967e2aa2e655c6c9ca2cb4a3abd9
Shinmera <shinmera@tymoon.eu>
2023-03-09 15:17:02 +0100

    Preliminary QOI support

---
+++ formats/qoi.lisp
@@ -0,0 +1,22 @@
+#|
+ This file is a part of trial
+ (c) 2017 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.shirakumo.fraf.trial)
+
+(defmethod load-image (path (type (eql :qoy)) &key)
+  (multiple-value-bind (image width height channels)
+      (with-open-file (stream path :element-type '(unsigned-byte 8))
+        (qoi:decode stream))
+    (flip-image-vertically image width height channels)
+    (values image
+            width
+            height
+            :unsigned-byte
+            (ecase channels
+              (1 :red)
+              (2 :rg)
+              (3 :rgb)
+              (4 :rgba)))))
---
+++ formats/trial-qoi.asd
@@ -0,0 +1,9 @@
+#|
+ This file is a part of trial
+ (c) 2016 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(asdf:defsystem trial-qoi
+  :components ((:file "qoi"))
+  :depends-on (:trial :qoi))
