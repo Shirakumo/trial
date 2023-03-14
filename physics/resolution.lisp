2650ae8d18128eb11029aa6ae663f3f17c207cca
Shinmera <shinmera@tymoon.eu>
2023-03-14 19:37:29 +0100

    yAAAAAAAAAAAA

--- physics/resolution.lisp
+++ physics/resolution.lisp
@@ -81,6 +81,10 @@
     (setf (contact-desired-delta hit) desired-delta)
     (setf (contact-a-relative hit) a-relative)
     (setf (contact-b-relative hit) b-relative)
+    (vsetf (contact-a-rotation-change hit) 0 0 0)
+    (vsetf (contact-a-velocity-change hit) 0 0 0)
+    (vsetf (contact-b-rotation-change hit) 0 0 0)
+    (vsetf (contact-b-velocity-change hit) 0 0 0)
     hit))
 
 (defun frictionless-impulse (contact &optional (impulse (vec3 0 0 0)))
@@ -201,6 +205,11 @@
     ;; Prepare Contacts
     (do-contacts (contact)      
       (upgrade-hit-to-contact contact dt))
+
+    (do-contacts (contact)
+      (format T "B ~a~%" i)
+      (format T "~a ~a~%" (contact-location contact) (contact-depth contact))
+      (format T "~a ~a~%" (location (contact-a contact)) (orientation (contact-a contact))))
     
     ;; Adjust Positions
     (loop repeat iterations
@@ -220,6 +229,11 @@
                      (* sign (v. (nv+ (vc rotation-change loc) velocity-change)
                                  (contact-normal other))))))
     
+    (do-contacts (contact)
+      (format T "C ~a~%" i)
+      (format T "~a ~a~%" (contact-location contact) (contact-depth contact))
+      (format T "~a ~a~%" (location (contact-a contact)) (orientation (contact-a contact))))
+    
     ;; Adjust Velocities
     (loop repeat iterations
           for worst = 0.01 ;; Some kinda epsilon.
@@ -231,14 +245,19 @@
                  (setf contact-i i)
                  (setf worst (contact-desired-delta contact))))
              (unless contact (loop-finish))
+             (format T "ROT1: ~a~%" (contact-a-rotation-change contact))
              (apply-velocity-change contact)
-             (format T "VEL: ~a~%" (contact-a-rotation-change contact))
+             (format T "ROT2: ~a~%" (contact-a-rotation-change contact))
              (do-update (rotation-change velocity-change loc sign)
                (let* ((delta (v+ (vc rotation-change loc) velocity-change))
                       (tmp (ntransform-inverse delta (contact-to-world other))))
                  (nv+* (contact-velocity other) tmp (- sign))
                  (setf (contact-desired-delta other)
-                       (desired-delta-velocity other (contact-velocity other) dt)))))))
+                       (desired-delta-velocity other (contact-velocity other) dt)))))
+
+    (format T "DONE.~%")
+    (format T "LOC: ~a ORI: ~a~%" (location (contact-a (aref contacts 0))) (orientation (contact-a (aref contacts 0))))
+    (format T "VEL: ~a ROT: ~a~%" (velocity (contact-a (aref contacts 0))) (rotation (contact-a (aref contacts 0))))))
 
 (defclass rigidbody-system (physics-system entity listener)
   ((contact-data :initform (make-contact-data) :accessor contact-data)))
@@ -250,6 +269,7 @@
     (loop for entity across objects
           do (loop for force across forces
                    do (apply-force force entity dt)))
+    (integrate rigidbody-system dt)
     (let ((data (contact-data rigidbody-system)))
       (setf (contact-data-start data) 0)
       ;; Compute contacts
@@ -271,5 +291,4 @@
               do (debug-line (contact-location contact)
                              (v+ (contact-location contact)
                                  (v* (contact-a-rotation-change contact)
-                                     10)))))))
-  (integrate rigidbody-system dt))
+                                     10))))))))
