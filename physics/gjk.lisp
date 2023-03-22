311900c4799e61b11af8f43e4661bcdc1fd5de31
Selwyn Simsek <selwyn.simsek@cantab.net>
2023-03-22 21:29:29 +0000

    Implement stub of GJK algorithm

--- physics/gjk.lisp
+++ physics/gjk.lisp
@@ -18,5 +18,27 @@
   (v- (support-mapping object-1 direction)
       (support-mapping object-2 (v- direction))))
 
+(defgeneric sample-one-point (object)
+  (:documentation
+   "Returns a single point from the object. Required for the GJK algorithm."))
+
+(defmethod sample-one-point ((sphere sphere)) (location sphere))
+
+(defun point-of-minimum-norm-in-convex-hull (set)
+  (error "implement me"))
+
+(defun reduce-to-smallest-spanning-set (p q)
+  "Returns the smallest subset of q such that p lies in the complex hull of q."
+  (error "implement me"))
+
 (defun gjk (a b)
-  )
+  (let ((q (vector (v- (sample-one-point a) (sample-one-point b))))) ; step 1
+    (loop do
+      (let ((p (point-of-minimum-norm-in-convex-hull q))) ; step 2
+        (when (< (v2norm (v- p (vec3 0.0 0.0 0.0))) 1e-6) ; step 3
+          (return-from gjk t))
+        (setf q (reduce-to-smallest-spanning-set p q)) ; step 4
+        (let ((v (support-mapping-minkowski-difference a b (v- p)))) ; step 5
+          (when (>= (v. v p) (v. p p)) ; step 6
+              (return-from gjk (values nil (v2norm p))))
+          (vector-push-extend v q)))))) ; step 7
