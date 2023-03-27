863812f7288c0b071549310b76a65b773c4d1ec0
Shinmera <shinmera@tymoon.eu>
2023-03-27 10:00:13 +0200

    Fix some crap

--- animation/workbench.lisp
+++ animation/workbench.lisp
@@ -1,30 +1,6 @@
-#|
- This file is a part of trial
- (c) 2022 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
- Author: Nicolas Hafner <shinmera@tymoon.eu>
-|#
+(in-package #:workbench)
 
-(defpackage #:org.shirakumo.fraf.trial.animation
-  (:use #:cl
-        #:org.shirakumo.flare.vector
-        #:org.shirakumo.flare.matrix
-        #:org.shirakumo.flare.quaternion
-        #:org.shirakumo.flare.transform)
-  (:local-nicknames
-   (#:trial #:org.shirakumo.fraf.trial)))
-
-(in-package #:org.shirakumo.fraf.trial.animation)
-
-(defclass workbench (trial:main) ()
-  (:default-initargs :clear-color (vec 0.25 0.3 0.35 0)
-                     :context '(:vsync T)))
-
-(defun launch (&rest args)
-  (apply #'trial:launch 'workbench args))
-
-(trial:define-pool workbench)
-
-(trial:define-asset (workbench simple) org.shirakumo.fraf.trial.gltf:asset
+(define-asset (workbench simple) org.shirakumo.fraf.trial.gltf:asset
     ;; #p "~/Projects/var/gltf-samples/2.0/Box/glTF/Box.gltf"
     ;; #p "~/Projects/var/gltf-samples/2.0/BoxTextured/glTF/BoxTextured.gltf"
     ;; #p "~/Projects/var/gltf-samples/2.0/SimpleSkin/glTF/SimpleSkin.gltf"
@@ -33,27 +9,24 @@
     #p "~/Projects/cl/trial-assets/data/Woman.gltf"
   )
 
-(trial:define-asset (workbench grid) trial:mesh
-    (trial:make-line-grid 10 10 10))
+(define-shader-entity player (animated-entity)
+  ((name :initform 'player)))
 
-(trial:define-shader-entity player (trial:animated-entity)
-  ((trial:name :initform 'player)))
-
-(trial:define-handler (player trial:tick :after) (trial:dt)
+(define-handler (player tick :after) (dt)
   (let ((vel (vec 0 0 0)))
-    (when (trial:retained :w) (incf (vz vel) 10.0))
-    (when (trial:retained :s) (decf (vz vel) 10.0))
+    (when (retained :w) (incf (vz vel) 10.0))
+    (when (retained :s) (decf (vz vel) 10.0))
     (if (v/= vel 0)
-        (trial:fade-to "Running" player)
-        (trial:fade-to "Idle" player))
-    (nv+ (tlocation (trial:tf player)) (nv* vel trial:dt))))
+        (fade-to "Running" player)
+        (fade-to "Idle" player))
+    (nv+ (tlocation (tf player)) (nv* vel dt))))
 
 (progn
-  (defmethod trial:setup-scene ((workbench workbench) scene)
-    (trial:disable :cull-face)
-    (trial:enter (make-instance 'trial::fps-counter) scene)
-    (trial:enter (make-instance 'trial:vertex-entity :vertex-array (trial:// 'workbench 'grid)) scene)
-    (trial:enter (make-instance 'player :asset (trial:asset 'workbench 'simple)) scene)
-    (trial:enter (make-instance 'trial:target-camera :target (vec 0 2 0) :location (vec 0 3 5)) scene)
-    (trial:enter (make-instance 'trial:render-pass) scene))
-  (trial:maybe-reload-scene))
+  (defmethod setup-scene ((workbench workbench) scene)
+    (disable :cull-face)
+    (enter (make-instance 'fps-counter) scene)
+    (enter (make-instance 'vertex-entity :vertex-array (// 'workbench 'grid)) scene)
+    (enter (make-instance 'player :asset (asset 'workbench 'simple)) scene)
+    (enter (make-instance 'target-camera :target (vec 0 2 0) :location (vec 0 3 5)) scene)
+    (enter (make-instance 'render-pass) scene))
+  (maybe-reload-scene))
