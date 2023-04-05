ff34fca71f13430bb31f4e2150873ed9e1d4ff58
Shinmera <shinmera@tymoon.eu>
2023-04-05 12:57:13 +0200

    Lol

--- main.lisp
+++ main.lisp
@@ -140,7 +140,7 @@
   (when (visible-p (context target))
     (blit-to-screen (scene source))))
 
-(defmethod launch ((main main) &rest initargs)
+(defmethod launch ((main symbol) &rest initargs)
   (standalone-logging-handler)
   (v:output-here)
   (v:info :trial.main "GENESIS")
