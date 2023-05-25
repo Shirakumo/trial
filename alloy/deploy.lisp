d025634cd76a08e90b79e1b814c1e0141db6aa1e
Shinmera <shinmera@tymoon.eu>
2023-05-25 10:12:24 +0200

    Fix shader file deployment

--- alloy/deploy.lisp
+++ alloy/deploy.lisp
@@ -10,7 +10,14 @@
   (deploy:status 1 "Copying fonts")
   (deploy:copy-directory-tree (org.shirakumo.alloy.renderers.opengl.msdf:fontcache-default-directory)
                               (pathname-utils:subdirectory directory "pool" "font-cache")
+                              :copy-root NIL)
+  (deploy:copy-directory-tree org.shirakumo.alloy.renderers.opengl::*shaders-directory*
+                              (pathname-utils:subdirectory directory "pool" "alloy")
                               :copy-root NIL))
+
+(deploy:define-hook (:boot alloy) (directory)
+  (setf org.shirakumo.alloy.renderers.opengl::*shaders-directory*
+        (pathname-utils:subdirectory directory "pool" "alloy")))
 
 #+linux
 (trial::dont-deploy
