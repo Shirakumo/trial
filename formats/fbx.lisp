7b1773070634acd298247a90ba6d6d8799ffe41b
Shinmera <shinmera@tymoon.eu>
2023-06-06 11:00:32 +0200

    FBX format stub.

---
+++ formats/fbx.lisp
@@ -0,0 +1,31 @@
+#|
+ This file is a part of trial
+ (c) 2017 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(defpackage #:org.shirakumo.fraf.trial.fbx
+  (:use #:cl+trial)
+  (:shadow #:asset)
+  (:local-nicknames
+   (#:fbx #:org.shirakumo.fraf.fbx))
+  (:export
+   #:asset))
+(in-package #:org.shirakumo.fraf.trial.fbx)
+
+(defclass asset (file-input-asset
+                 multi-resource-asset
+                 animation-asset
+                 trial::full-load-asset)
+  ())
+
+(defmethod generate-resources ((asset asset) input &key)
+  (fbx:with-freeing ((axes (make-instance 'fbx:coordinate-axes
+                                          :right :positive-x
+                                          :up :positive-y
+                                          :front :negative-z))
+                     (file (fbx:parse input :generate-missing-normals T
+                                            :normalize-normals T
+                                            :target-axes axes)))
+    ;; FIXME: do this some day.
+    ))
---
+++ formats/trial-fbx.asd
@@ -0,0 +1,9 @@
+#|
+ This file is a part of trial
+ (c) 2016 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(asdf:defsystem trial-fbx
+  :components ((:file "fbx"))
+  :depends-on (:trial :cl-fbx))
