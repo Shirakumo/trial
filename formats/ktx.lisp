36fc016c22356bedac98a1adc27666254ace7173
Shinmera <shinmera@tymoon.eu>
2023-06-16 15:12:41 +0200

    Trivial KTX integration

---
+++ formats/ktx.lisp
@@ -0,0 +1,30 @@
+#|
+ This file is a part of trial
+ (c) 2017 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.shirakumo.fraf.trial)
+
+(defmethod load-image (path (type (eql :ktx)))
+  (let ((ktx (org.shirakumo.ktx:read-file path)))
+    (loop for mip across (org.shirakumo.ktx:mipmaps ktx)
+          for level from 0
+          collect (make-image-source (org.shirakumo.ktx:data mip)
+                                     (org.shirakumo.ktx:width ktx)
+                                     (org.shirakumo.ktx:height ktx)
+                                     (org.shirakumo.ktx:gl-type ktx)
+                                     (org.shirakumo.ktx:gl-format ktx)
+                                     :level level))))
+
+(defmethod save-image ((source texture-source) target (type (eql :ktx)) &key)
+  (destructuring-bind (x y z w h d) (texture-source-src source)
+    (declare (ignore x y z))
+    (let ((ktx (org.shirakumo.ktx:create-file
+                (vector (pixel-data source))
+                (pixel-type source)
+                (pixel-format source)
+                :width w :height (or h 0) :depth (or d 0))))
+      (org.shirakumo.ktx:write-file ktx target))))
+
+;; TODO: ICO to decode to mips? maybe?
---
+++ formats/trial-ktx.asd
@@ -0,0 +1,9 @@
+#|
+ This file is a part of trial
+ (c) 2016 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(asdf:defsystem trial-ktx
+  :components ((:file "ktx"))
+  :depends-on (:trial :cl-ktx))
