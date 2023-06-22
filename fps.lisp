acfa715503c68b29861fe1672d9520f1b6dc1545
Shinmera <shinmera@tymoon.eu>
2023-06-22 18:28:19 +0200

    Fix fps counter

--- fps.lisp
+++ fps.lisp
@@ -75,7 +75,7 @@
 (define-class-shader (fps-counter :vertex-shader)
   "
 layout (location = 0) in vec3 position;
-layout (location = 1) in vec2 in_uv;
+layout (location = 2) in vec2 in_uv;
 out vec2 uv;
 
 void main(){
