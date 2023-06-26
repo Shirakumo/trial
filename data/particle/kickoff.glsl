5b5d3e015852f702e205dbed72972d18e837d4c0
Shinmera <shinmera@tymoon.eu>
2023-06-26 14:07:28 +0200

    More particle work. Almost there.

--- data/particle/kickoff.glsl
+++ data/particle/kickoff.glsl
@@ -7,8 +7,8 @@ void main(){
   uint alive_new = draw_args.x / 6;
   uint real_emit = min(dead, emit_count);
   
-  emit_args = uvec3(ceil((float)(real_emit) / (float)(EMIT_THREADS)), 1, 1);
-  simulate_args = uvec3(ceil((float)(alive_new+real_emit) / (float)(SIMULATE_THREADS)), 1, 1);
+  emit_args = uvec3(ceil(float(real_emit) / float(EMIT_THREADS)), 1, 1);
+  simulate_args = uvec3(ceil(float(alive_new+real_emit) / float(SIMULATE_THREADS)), 1, 1);
   draw_args = uvec4(0, 1, 0, 0);
 
   alive_count = alive_new;
