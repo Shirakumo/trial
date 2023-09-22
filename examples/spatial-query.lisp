7649583bb9670993409c10e5afde40a82d352fd5
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2023-09-22 22:02:44 +0200

    Rename accessors of SPATIAL-STRUCTURE to something less easily confused
    Also increase the number of boxes and the size of the test volume a
    bit.

--- examples/spatial-query.lisp
+++ examples/spatial-query.lisp
@@ -28,54 +28,56 @@
     (gl:polygon-mode :front-and-back :fill)))
 
 (defclass spatial-structure (trial:array-container listener)
-  ((q :initarg :q  :accessor q)
-   (f :initarg :f :initform (make-instance 'ubox* :scaling (vec3 0.5)) :accessor f)
+  ((spatial-index :initarg :spatial-index  :accessor spatial-index)
+   (query-region :initarg :query-region :initform (make-instance 'ubox* :scaling (vec3 1)) :accessor query-region)
    (dirty :initform NIL :accessor dirty)))
 
 (defmethod initialize-instance :after ((structure spatial-structure) &key)
-  (enter (f structure) structure))
+  (enter (query-region structure) structure))
 
 (defmethod enter :after ((box ubox) (structure spatial-structure))
   (setf (dirty structure) T)
-  (unless (eql box (f structure))
-    (spaces:enter box (q structure))))
+  (unless (eql box (query-region structure))
+    (spaces:enter box (spatial-index structure))))
 
 (defmethod leave :after ((box ubox) (structure spatial-structure))
   (setf (dirty structure) T)
-  (spaces:leave box (q structure)))
+  (spaces:leave box (spatial-index structure)))
 
 (defmethod clear :after ((structure spatial-structure))
   (setf (dirty structure) T)
-  (spaces:clear (q structure)))
+  (spaces:clear (spatial-index structure)))
 
-(defmethod (setf q) :after ((q spaces:container) (structure spatial-structure))
+(defmethod (setf spatial-index) :after ((spatial-index spaces:container) (structure spatial-structure))
   (setf (dirty structure) T)
   (for:for ((entity over structure))
-    (unless (eql entity (f structure))
-      (spaces:enter entity q))))
+    (unless (eql entity (query-region structure))
+      (spaces:enter entity spatial-index))))
 
 (defmethod start-frame ((structure spatial-structure))
-  (spaces:do-all (e (q structure))
-    (vsetf (color e) 1 0 0 0.2))
-  (spaces:do-overlapping (e (q structure) (f structure))
-    (vsetf (color e) 0 1 0 0.2))
-  (spaces:do-contained (e (q structure) (f structure))
-    (vsetf (color e) 1 1 0 0.2))
+  (let ((spatial-index (spatial-index structure))
+        (query-region (query-region structure)))
+    (spaces:do-all (e spatial-index)
+      (vsetf (color e) 1 0 0 .1))
+    (spaces:do-overlapping (e spatial-index query-region)
+      (vsetf (color e) 1 1 0 1))
+    (spaces:do-contained (e spatial-index query-region)
+      (vsetf (color e) 0 1 0 1)))
   (setf (dirty structure) NIL))
 
 (defmethod bsize ((structure spatial-structure))
-  (scaling (f structure)))
+  (scaling (query-region structure)))
 
 (defmethod location ((structure spatial-structure))
-  (location (f structure)))
+  (location (query-region structure)))
 
 (defmethod (setf bsize) ((vec vec3) (structure spatial-structure))
   (setf (dirty structure) T)
-  (setf (scaling (f structure)) vec))
+  (setf (scaling (query-region structure)) vec))
 
 (defmethod (setf location) ((vec vec3) (structure spatial-structure))
   (setf (dirty structure) T)
-  (setf (location (f structure)) vec))
+  (setf (location (query-region structure)) vec))
 
 (define-handler (spatial-structure tick) ()
   (when (dirty spatial-structure)
@@ -99,10 +101,10 @@
     (let ((size (alloy:represent cube-size 'alloy:ranged-wheel :range '(0.001 . 0.5) :step 0.01 :layout-parent layout :focus-parent focus)))
       (alloy:on alloy:value (value size)
         (for:for ((entity over structure))
-          (unless (eql entity (f structure))
+          (unless (eql entity (query-region structure))
             (setf (scaling entity) (vec3 cube-size))))
-        (spaces:clear (q structure))
-        (setf (q structure) (q structure))))
+        (spaces:clear (spatial-index structure))
+        (setf (spatial-index structure) (spatial-index structure))))
     (alloy:finish-structure panel layout focus)))
 
 (define-example spatial-query
@@ -113,11 +115,11 @@
     (connect (port ui 'color) (port combine 'b-pass) scene))
   (enter (make-instance 'pivot-camera :radius 2.5) scene)
   (let ((structure (make-instance 'spatial-structure
-                                  :q (ecase :kd
-                                       (:kd (org.shirakumo.fraf.trial.space.kd-tree:make-kd-tree))
-                                       (:grid (org.shirakumo.fraf.trial.space.grid3:make-grid 0.01 :bsize (vec3 1)))))))
+                                  :spatial-index (ecase :kd
+                                                   (:kd (org.shirakumo.fraf.trial.space.kd-tree:make-kd-tree))
+                                                   (:grid (org.shirakumo.fraf.trial.space.grid3:make-grid 0.01 :bsize (vec3 1)))))))
     (enter structure scene)
-    (loop for i from 0 below 1000
-          for box = (make-instance 'ubox :location (vrand (vec3 0) (vec3 2)) :scaling (vec3 0.01))
+    (loop for i from 0 below 10000
+          for box = (make-instance 'ubox :location (vrand (vec3 0) (vec3 6)) :scaling (vec3 0.05))
           do (enter box structure))
     (trial-alloy:show-panel 'query-panel :structure structure)))
