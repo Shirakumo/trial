0c3f030036ab19bed0f9d2d7dd580f89fcb073c6
Shinmera <shinmera@tymoon.eu>
2023-10-22 13:09:34 +0200

    Use our own turbojpeg lib.

--- formats/jpeg-turbo.lisp
+++ formats/jpeg-turbo.lisp
@@ -1,62 +1,27 @@
 (in-package #:org.shirakumo.fraf.trial)
 
-(defmethod load-image ((path pathname) (type (eql :jpeg)))
-  (jpeg-turbo:with-decompressor (handle)
-    (multiple-value-bind (image width height) (jpeg-turbo:decompress handle path :pixel-format :rgb :flags '(:bottomup))
-      (make-image-source image width height :unsigned-byte :rgb))))
-
-(defun decompress-header-from-pointer (handle ptr size)
-  (cffi:with-foreign-objects ((width      :int 1)
-                              (height     :int 1)
-                              (subsamp    :int 1)
-                              (colorspace :int 1))
-    (let ((code (cffi:foreign-funcall "tjDecompressHeader3"
-                                      :pointer handle
-                                      :pointer ptr
-                                      :ulong size
-                                      :pointer width
-                                      :pointer height
-                                      :pointer subsamp
-                                      :pointer colorspace
-                                      :int)))
-      (if (zerop code)
-          (values (cffi:mem-aref width  :int) (cffi:mem-aref height :int))
-          (error 'jpeg-turbo:jpeg-error :error-string (jpeg-turbo::last-error handle))))))
-
-(defun decompress-from-pointer (handle ptr size &key (scaling-factor 1) (pixel-format :rgb) flags)
-  (multiple-value-bind (width height) (decompress-header-from-pointer handle ptr size)
-    (setq width  (jpeg-turbo::scale width  scaling-factor)
-          height (jpeg-turbo::scale height scaling-factor))
-    (let ((region (mem:allocate NIL (* width height (jpeg-turbo::pixel-size pixel-format)))))
-      (let ((code (cffi:foreign-funcall "tjDecompress2"
-                                        :pointer handle
-                                        :pointer ptr
-                                        :ulong size
-                                        :pointer (memory-region-pointer region)
-                                        :int width
-                                        :int 0
-                                        :int height
-                                        jpeg-turbo::pixel-format pixel-format
-                                        jpeg-turbo::jpeg-turbo-flags (cons :no-realloc flags)
-                                        :int)))
-        (if (zerop code)
-            (values region width height)
-            (error 'jpeg-turbo:jpeg-error :error-string (jpeg-turbo::last-error handle)))))))
-
 (defmethod load-image (source (type (eql :jpeg)))
   (mem:with-memory-region (region source)
-    (jpeg-turbo:with-decompressor (handle)
-      (multiple-value-bind (image width height) (decompress-from-pointer handle (memory-region-pointer region) (memory-region-size region)
-                                                                         :pixel-format :rgb :flags '(:bottomup))
-        (make-image-source image width height :unsigned-byte :rgb)))))
+    (multiple-value-bind (buffer width height)
+        (org.shirakumo.fraf.turbojpeg:load-image (mem:memory-region-pointer region) T
+                                                 :size (mem:memory-region-size region)
+                                                 :pixel-format :rgb
+                                                 :buffer :vector)
+      (make-image-source buffer width height :unsigned-byte :rgb))))
 
 (defmethod load-image (path (type (eql :jpg)))
   (load-image path :jpeg))
 
-;; (defmethod save-image ((source texture-source) (path pathname) (type (eql :jpeg)) &key)
-;;   (destructuring-bind (x y z w h d) (texture-source-src source)
-;;     (declare (ignore x y z d))
-;;     ))
+(defmethod save-image ((source texture-source) destination (type (eql :jpeg)) &key)
+  (destructuring-bind (x y z w h d) (texture-source-src source)
+    (declare (ignore x y z d))
+    (mem:with-memory-region (region destination)
+      (org.shirakumo.fraf.turbojpeg:save-image (mem:memory-region-pointer region) (texture-source-pixel-data source) w h T
+                                               :size (mem:memory-region-size region)
+                                               :pixel-format (texture-source-pixel-format source)
+                                               :bit-depth (ecase (texture-source-pixel-type source)
+                                                            (:unsigned-byte 8)
+                                                            (:unsigned-short 16))))))
 
-;; (defmethod save-image (source target (type (eql :jpg)) &rest args)
-;;   (apply #'save-image source target :jpeg args))
+(defmethod save-image (source target (type (eql :jpg)) &rest args)
+  (apply #'save-image source target :jpeg args))
--- formats/trial-jpeg-turbo.asd
+++ formats/trial-jpeg-turbo.asd
@@ -1,3 +1,3 @@
 (asdf:defsystem trial-jpeg-turbo
   :components ((:file "jpeg-turbo"))
-  :depends-on (:trial :jpeg-turbo))
+  :depends-on (:trial :cl-turbojpeg))
