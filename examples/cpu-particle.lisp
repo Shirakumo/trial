900032b35a388f0c1fd32b8b70a4a371d68e08e6
Shinmera <shinmera@tymoon.eu>
2024-01-29 13:11:02 +0100

    Less shitty cpu particle workbench

--- examples/cpu-particle.lisp
+++ examples/cpu-particle.lisp
@@ -16,3 +16,27 @@
   (enter (make-instance 'vertex-entity :vertex-array (// 'trial 'grid)) scene)
   (enter (make-instance 'editor-camera :location (VEC3 0.0 2.3 7.3) :fov 50 :move-speed 0.1) scene)
   (enter (make-instance 'phong-render-pass) scene))
+
+(defmethod setup-ui ((scene cpu-particle-scene) panel)
+  (let ((layout (make-instance 'alloy:grid-layout :col-sizes '(T 120 200) :row-sizes '(30)))
+        (focus (make-instance 'alloy:vertical-focus-list))
+        (emitter (node :emitter scene))
+        (row -1))
+    (macrolet ((wheel (place title start end &rest args)
+                 `(progn
+                    (alloy:enter ,title layout :row (incf row) :col 1)
+                    (alloy:represent (,place emitter) 'alloy:ranged-wheel
+                                     :range '(,start . ,end) ,@args :layout-parent layout :focus-parent focus))))
+      (wheel particle-rate "Particle Rate" 0 200)
+      (wheel particle-lifespan "Lifespan" 0.0 100.0)
+      (wheel particle-lifespan-randomness "Lifespan Random" 0.0 1.0)
+      (wheel particle-velocity "Velocity" 0.0 100.0)
+      (wheel particle-randomness "Randomness" 0.0 1.0)
+      (wheel particle-size "Size" 0.01 10.0)
+      (wheel particle-scaling "Scaling" 0.0 10.0)
+      (alloy:enter "Texture" layout :row (incf row) :col 1)
+      (alloy:represent (texture emitter) T :layout-parent layout :focus-parent focus)
+      (alloy:enter "Blend Mode" layout :row (incf row) :col 1)
+      (alloy:represent (blend-mode emitter) 'alloy:combo-set
+                       :value-set '(:add :normal :invert :darken :multiply :screen) :layout-parent layout :focus-parent focus))
+    (alloy:finish-structure panel layout focus)))
