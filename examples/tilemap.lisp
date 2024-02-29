a7f29d67770692b8754beae218a90d7797fda2d4
Shinmera <shinmera@tymoon.eu>
2024-02-29 13:47:25 +0100

    Minor

--- examples/tilemap.lisp
+++ examples/tilemap.lisp
@@ -3,5 +3,5 @@
 (define-example tilemap
   :title "Tile Maps"
   (enter (make-instance 'tile-layer :tile-data (assets:asset :tilemap) :name :tilemap) scene)
-  (enter (make-instance 'sidescroll-camera :zoom 2.0 :target (node :sprite scene)) scene)
+  (enter (make-instance 'sidescroll-camera :zoom 2.0) scene)
   (enter (make-instance 'render-pass) scene))
