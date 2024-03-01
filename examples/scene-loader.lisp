8e9a7f81940b910b7332ac2d71dc0d923be0b742
Shinmera <shinmera@tymoon.eu>
2024-03-01 09:57:14 +0100

    More scene-loader fixes

--- examples/scene-loader.lisp
+++ examples/scene-loader.lisp
@@ -14,8 +14,8 @@
   (enter (make-instance 'display-controller) scene)
   (enter (make-instance 'gravity :gravity (vec 0 -10 0)) scene)
   (observe! (paused-p scene) :title "Pause [P]")
-  (let ((render (make-instance 'pbr-render-pass))
-        (map (make-instance 'ward)))
+  (let ((render (make-instance 'pbr-render-pass :name :render))
+        (map (make-instance 'ward :name :map)))
     (connect (port render 'color) (port map 'previous-pass) scene))
   (setf (file scene) (input* (assets:asset :physics-test))))
 
@@ -26,7 +26,9 @@
       (alloy:enter button layout :row 0 :col 2)
       (alloy:on alloy:activate (button)
         (let ((file (org.shirakumo.file-select:existing :title "Load Model File..."
-                                                        :filter '(("glTF" "glb" "gltf"))
+                                                        :filter '(("Model Files" "glb" "gltf" "obj")
+                                                                  ("glTF" "glb" "gltf")
+                                                                  ("Wavefront OBJ" "obj"))
                                                         :default (file scene))))
           (when file (setf (file scene) file))))
 
@@ -42,39 +44,27 @@
 
 (defmethod (setf file) :after (file (scene scene-loader-scene))
   (unless (incremental-load scene)
-    (for:for ((entity over scene))
+    (sequence:dosequence (entity scene)
       (when (typep entity '(and scene-node (not controller)))
         (leave entity T))))
   (setf (paused-p scene) T)
   (generate-resources 'model-file file :load-scene T)
-  (commit scene (loader +main+))
   (unless (do-scene-graph (node scene)
             (when (typep node 'light) (return T)))
-    (enter (make-instance 'directional-light :direction -vy3+) scene)
-    (enter (make-instance 'ambient-light :color (vec3 0.5)) scene))
+    (enter (make-instance 'directional-light :direction -vy3+ :color (vec3 3)) scene)
+    (enter (make-instance 'ambient-light :color (vec3 0.25)) scene))
   (let ((camera (do-scene-graph (node scene)
                   (when (typep node 'camera) (return node)))))
     (if camera
         (setf (camera scene) camera)
         (enter (make-instance 'editor-camera :name :camera :location (VEC3 10.0 20 14) :rotation (vec3 0.75 5.5 0.0) :fov 50 :move-speed 0.1) scene)))
   (enter (make-instance 'vertex-entity :name :grid :vertex-array (// 'trial 'grid)) scene)
-  ;; FIXME: auto-fit camera to model
   (commit scene (loader +main+)))
 
 (define-handler ((scene scene-loader-scene) text-entered :after) (text)
   (case (char text 0)
     (#\p (setf (paused-p scene) (not (paused-p scene))))
-    (#\r (setf (file scene) (file scene)))
-    (#\n (let ((cube (find "Cube.001"
-                           (find "Scene" scene :test 'equal :key 'name)
-                           :test 'equal :key 'name)))
-           (when cube
-             (trotate-by (trial:tf cube) (vec3 1 0 0) 0.1))))
-    (#\m (let ((cube (find "Cube.001"
-                           (find "Scene" scene :test 'equal :key 'name)
-                           :test 'equal :key 'name)))
-           (when cube
-             (trotate-by (trial:tf cube) (vec3 1 0 0) 0.02))))))
+    (#\r (setf (file scene) (file scene)))))
 
 (define-handler ((scene scene-loader-scene) (ev tick) :around) ()
   (cond ((paused-p scene)
