baed9773e372a7149f63fd9c17b0363a505313a8
Shinmera <shinmera@tymoon.eu>
2024-03-04 17:19:30 +0100

    Ech.

--- data/ssr.glsl
+++ data/ssr.glsl
@@ -2,10 +2,9 @@
 
 uniform sampler2D depth_map;
 uniform float ssr_max_distance = 15.0;
-uniform float ssr_ray_resolution = 0.3;
 uniform float ssr_distance_falloff = 0.0;
 uniform int ssr_step_count = 20;
-uniform float ssr_thickness = 0.2;
+uniform float ssr_thickness = 0.3;
 
 vec3 evaluate_ssr(in vec2 uv, in vec3 normal){
   vec3 start_pos = depth_world_pos(depth_map, uv, inv_projection_matrix);
@@ -13,25 +12,29 @@ vec3 evaluate_ssr(in vec2 uv, in vec3 normal){
   vec3 end_pos = start_pos + ray_dir * ssr_max_distance;
   vec3 ray_step = ray_dir * (ssr_max_distance / ssr_step_count);
   vec3 ray = start_pos;
+  vec2 ray_uv;
 
-  float depth_step = abs(ray_step.z);
+  // 1. Find the texel on which we intersect
   for(int i=0; i<ssr_step_count; ++i){
     ray += ray_step;
-    vec2 ray_uv = to_clip_uv(ray, projection_matrix);
+    ray_uv = to_clip_uv(ray, projection_matrix);
     vec3 depth_pos = depth_world_pos(depth_map, ray_uv, inv_projection_matrix);
 
-    float depth_diff = (ray.z - depth_pos.z);
-    if(0 < depth_diff && depth_diff < ssr_thickness+depth_step){
-      vec2 border_falloff = 1-clamp(abs(5-ray_uv*10)-4, 0.0, 1.0);
-
-      float visibility
-        = (1 - max(0.0, dot(-normalize(start_pos), ray_dir)))
-        * (1 - distance(end_pos, ray) / ssr_max_distance)
-        * (1 - clamp(depth_diff / ssr_thickness, 0.0, 1.0))
-        * min(border_falloff.x, border_falloff.y);
-
-      return vec3(ray_uv, visibility);
+    float depth_diff = abs(ray.z - depth_pos.z) * (ssr_step_count / ssr_max_distance) * 0.5;
+    if(depth_diff < ssr_thickness){
+      break;
     }
   }
-  return vec3(0);
+
+  // 2. Interpolate to get a smooth uv transition
+  
+
+  // 3. Compute visibility factor to blend off reflections
+  vec2 border_falloff = 1-clamp(abs(5-ray_uv*10)-4, 0.0, 1.0);
+
+  float visibility
+    = (max(0.0, dot(normalize(start_pos), ray_dir)))
+    * mix(1.0, distance(end_pos, ray) / ssr_max_distance, ssr_distance_falloff)
+    * min(border_falloff.x, border_falloff.y);
+  return vec3(ray_uv, visibility);
 }
--- data/standard-render-pbr-ssr.glsl
+++ data/standard-render-pbr-ssr.glsl
@@ -6,8 +6,6 @@ uniform sampler2D previous_pass;
 void standard_finish@after(){
   vec2 uv = gl_FragCoord.xy / view_size;
   vec3 ref = evaluate_ssr(uv, mat3(view_matrix) * normal);
-  //vec3 reflection_color = pow(texture(previous_pass, ref.xy).rgb, vec3(gamma));
-  //color.rgb = mix(color.rgb, reflection_color, 1.0);
-  //color.rgb = texture(previous_pass, uv).rgb;
-  color.rgb = vec3(ref);
+  vec3 reflection_color = texture(previous_pass, ref.xy).rgb;
+  color.rgb = mix(color.rgb, reflection_color, ref.z);
 }
