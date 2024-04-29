8233f1d96f715854864c09b2a6c93e2a6121c9bb
Shinmera <shinmera@tymoon.eu>
2024-04-29 11:29:04 +0200

    Dumb

--- data/random.glsl
+++ data/random.glsl
@@ -6,13 +6,13 @@ float random(in float x) {
 }
 
 float random(in vec2 st) {
-  vec3 p3  = fract(vec3(st.xyx) * vec3(443.897, 441.423, .0973, .1099));
+  vec3 p3  = fract(vec3(st.xyx) * vec3(443.897, 441.423, .0973));
   p3 += dot(p3, p3.yzx + 33.33);
   return fract((p3.x + p3.y) * p3.z);
 }
 
 float random(in vec3 pos) {
-  pos  = fract(pos * vec3(443.897, 441.423, .0973, .1099));
+  pos  = fract(pos * vec3(443.897, 441.423, .0973));
   pos += dot(pos, pos.zyx + 31.32);
   return fract((pos.x + pos.y) * pos.z);
 }
@@ -24,37 +24,37 @@ float random(in vec4 pos) {
 }
 
 vec2 random2(float p) {
-  vec3 p3 = fract(vec3(p) * vec3(443.897, 441.423, .0973, .1099));
+  vec3 p3 = fract(vec3(p) * vec3(443.897, 441.423, .0973));
   p3 += dot(p3, p3.yzx + 19.19);
   return fract((p3.xx + p3.yz) * p3.zy);
 }
 
 vec2 random2(vec2 p) {
-  vec3 p3 = fract(p.xyx * vec3(443.897, 441.423, .0973, .1099));
+  vec3 p3 = fract(p.xyx * vec3(443.897, 441.423, .0973));
   p3 += dot(p3, p3.yzx + 19.19);
   return fract((p3.xx + p3.yz) * p3.zy);
 }
 
 vec2 random2(vec3 p3) {
-  p3 = fract(p3 * vec3(443.897, 441.423, .0973, .1099));
+  p3 = fract(p3 * vec3(443.897, 441.423, .0973));
   p3 += dot(p3, p3.yzx + 19.19);
   return fract((p3.xx + p3.yz) * p3.zy);
 }
 
 vec3 random3(float p) {
-  vec3 p3 = fract(vec3(p) * vec3(443.897, 441.423, .0973, .1099));
+  vec3 p3 = fract(vec3(p) * vec3(443.897, 441.423, .0973));
   p3 += dot(p3, p3.yzx + 19.19);
   return fract((p3.xxy + p3.yzz) * p3.zyx); 
 }
 
 vec3 random3(vec2 p) {
-  vec3 p3 = fract(vec3(p.xyx) * vec3(443.897, 441.423, .0973, .1099));
+  vec3 p3 = fract(vec3(p.xyx) * vec3(443.897, 441.423, .0973));
   p3 += dot(p3, p3.yxz + 19.19);
   return fract((p3.xxy + p3.yzz) * p3.zyx);
 }
 
 vec3 random3(vec3 p) {
-  p = fract(p * vec3(443.897, 441.423, .0973, .1099));
+  p = fract(p * vec3(443.897, 441.423, .0973));
   p += dot(p, p.yxz + 19.19);
   return fract((p.xxy + p.yzz) * p.zyx);
 }
