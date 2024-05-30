dd9364afae1f1e77775aeacb73bcb1489564d440
Shinmera <shinmera@tymoon.eu>
2024-05-30 20:12:37 +0200

    Cache Alloy shaders in memory on deploy

--- alloy/deploy.lisp
+++ alloy/deploy.lisp
@@ -5,9 +5,10 @@
   (deploy:copy-directory-tree (org.shirakumo.alloy.renderers.opengl.msdf:fontcache-default-directory)
                               (pathname-utils:subdirectory directory "pool" "font-cache")
                               :copy-root NIL)
-  (deploy:copy-directory-tree org.shirakumo.alloy.renderers.opengl::*shaders-directory*
-                              (pathname-utils:subdirectory directory "pool" "alloy")
-                              :copy-root NIL))
+  (deploy:status 1 "Caching Alloy shaders in-memory")
+  (org.shirakumo.alloy.renderers.opengl::cache-shader-source-files)
+  (setf (fdefinition 'org.shirakumo.alloy.renderers.opengl::shader-source)
+        #'org.shirakumo.alloy.renderers.opengl::shader-source-from-cache))
 
 (deploy:define-hook (:boot alloy) (directory)
   (setf org.shirakumo.alloy.renderers.opengl::*shaders-directory*
