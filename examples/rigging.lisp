3372336ca53982921df4bf0fa7817aa76bf7e177
Shinmera <shinmera@tymoon.eu>
2024-06-26 22:14:04 +0200

    minor

--- examples/rigging.lisp
+++ examples/rigging.lisp
@@ -8,8 +8,8 @@
     (when (retained :w) (incf (vz vel) 10.0))
     (when (retained :s) (decf (vz vel) 10.0))
     (if (v/= vel 0)
-        (fade-to "Running" entity)
-        (fade-to "Idle" entity))
+        (fade-to :Running entity)
+        (fade-to :Idle entity))
     (nv+ (tlocation (tf entity)) (nv* vel dt))
     (handle tick (animation-controller entity))))
 
