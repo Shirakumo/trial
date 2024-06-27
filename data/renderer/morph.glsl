f657deab9157064e61528dcbfe12b48408406f51
Shinmera <shinmera@tymoon.eu>
2024-06-27 17:36:57 +0200

    Clamp morphs shader-side to avoid bad data fucking shit up

--- data/renderer/morph.glsl
+++ data/renderer/morph.glsl
@@ -1,18 +1,18 @@
 // We need to include it as a uniform, but the struct include
 // would not create the correct uniform block.
 // #include trial::morph-data
-
+#define SIMULTANEOUS_MORPHS 8
 layout(std140) uniform MorphData {
   int count;
-  float weights[8];
-  int indices[8];
+  float weights[SIMULTANEOUS_MORPHS];
+  int indices[SIMULTANEOUS_MORPHS];
 } morph_data;
 
 uniform sampler1DArray morph_targets;
 
 void morph_vertex(inout vec3 position, inout vec3 normal, inout vec2 uv){
   int vid = gl_VertexID*3;
-  for(int i=0; i<morph_data.count; ++i){
+  for(int i=0; i<min(morph_data.count, SIMULTANEOUS_MORPHS); ++i){
     int index = morph_data.indices[i];
     float weight = morph_data.weights[i];
     position += weight * texelFetch(morph_targets, ivec2(index, vid+0), 0).xyz;
