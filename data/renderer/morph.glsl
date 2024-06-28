13fff61000d02ec0aaba0a1cd0457b0229268149
Shinmera <shinmera@tymoon.eu>
2024-06-28 21:33:43 +0200

    FINALLY make it all work

--- data/renderer/morph.glsl
+++ data/renderer/morph.glsl
@@ -15,8 +15,8 @@ void morph_vertex(inout vec3 position, inout vec3 normal, inout vec2 uv){
   for(int i=0; i<min(morph_data.count, SIMULTANEOUS_MORPHS); ++i){
     int index = morph_data.indices[i];
     float weight = morph_data.weights[i];
-    position += weight * texelFetch(morph_targets, ivec2(index, vid+0), 0).xyz;
-    normal += weight * texelFetch(morph_targets, ivec2(index, vid+1), 0).xyz;
-    uv += weight * texelFetch(morph_targets, ivec2(index, vid+2), 0).xy;
+    position += weight * texelFetch(morph_targets, ivec2(vid+0, index), 0).xyz;
+    normal += weight * texelFetch(morph_targets, ivec2(vid+1, index), 0).xyz;
+    uv += weight * texelFetch(morph_targets, ivec2(vid+2, index), 0).xy;
   }
 }
