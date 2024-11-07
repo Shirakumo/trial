2817136c8eecebb89ba28d8faa48af57a821490a
Yukari Hafner <shinmera@tymoon.eu>
2024-11-07 21:28:16 +0100

    Fix the kawase implementation

--- data/post/kawase.glsl
+++ data/post/kawase.glsl
@@ -1,4 +1,5 @@
 #section FRAGMENT_SHADER
+in vec2 uv;
 out vec4 color;
 uniform sampler2D previous_pass;
 uniform vec2 resolution;
@@ -6,9 +7,9 @@ uniform float intensity = 3.0;
 uniform int down = 1;
 
 void main(){
+  vec2 uv = gl_FragCoord.xy / resolution;
+  vec2 off = (intensity*0.5) / resolution;
   if(0 < down){
-    vec2 uv = gl_FragCoord.xy / resolution;
-    vec2 off = intensity*0.5 / resolution;
     vec3 sum = texture(previous_pass, uv).rgb * 4.0;
     sum += texture(previous_pass, uv + vec2(-1, -1) * off).rgb;
     sum += texture(previous_pass, uv + vec2(+1, +1) * off).rgb;
@@ -16,8 +17,6 @@ void main(){
     sum += texture(previous_pass, uv + vec2(-1, +1) * off).rgb;
     color = vec4(sum * 0.125, 1);
   }else{
-    vec2 uv = gl_FragCoord.xy / resolution;
-    vec2 off = intensity*0.5 / resolution;
     vec3 sum = texture(previous_pass, uv + vec2(-2, +0) * off).rgb;
     sum += texture(previous_pass, uv + vec2(-1, +1) * off).rgb * 2.0;
     sum += texture(previous_pass, uv + vec2(+0, +2) * off).rgb;
