e53f5bd3d2a41966699279b9d614f3787965cedd
Yukari Hafner <shinmera@tymoon.eu>
2024-11-08 09:35:09 +0100

    Sobel: ignore alpha

--- data/post/sobel.glsl
+++ data/post/sobel.glsl
@@ -8,15 +8,15 @@ void main(){
   ivec2 size = textureSize(previous_pass, 0);
   float sobelSizeH = intensity / size.x;
   float sobelSizeV = intensity / size.y;
-  vec4 top         = texture(previous_pass, vec2(uv.x, uv.y + sobelSizeV));
-  vec4 bottom      = texture(previous_pass, vec2(uv.x, uv.y - sobelSizeV));
-  vec4 left        = texture(previous_pass, vec2(uv.x - sobelSizeH, uv.y));
-  vec4 right       = texture(previous_pass, vec2(uv.x + sobelSizeH, uv.y));
-  vec4 topLeft     = texture(previous_pass, vec2(uv.x - sobelSizeH, uv.y + sobelSizeV));
-  vec4 topRight    = texture(previous_pass, vec2(uv.x + sobelSizeH, uv.y + sobelSizeV));
-  vec4 bottomLeft  = texture(previous_pass, vec2(uv.x - sobelSizeH, uv.y - sobelSizeV));
-  vec4 bottomRight = texture(previous_pass, vec2(uv.x + sobelSizeH, uv.y - sobelSizeV));
-  vec4 sx = -topLeft + -2 * left + -bottomLeft + topRight   + 2 * right  + bottomRight;
-  vec4 sy = -topLeft + -2 * top  + -topRight   + bottomLeft + 2 * bottom + bottomRight;
-  color = sqrt(sx * sx + sy * sy);
+  vec3 top         = texture(previous_pass, vec2(uv.x, uv.y + sobelSizeV)).rgb;
+  vec3 bottom      = texture(previous_pass, vec2(uv.x, uv.y - sobelSizeV)).rgb;
+  vec3 left        = texture(previous_pass, vec2(uv.x - sobelSizeH, uv.y)).rgb;
+  vec3 right       = texture(previous_pass, vec2(uv.x + sobelSizeH, uv.y)).rgb;
+  vec3 topLeft     = texture(previous_pass, vec2(uv.x - sobelSizeH, uv.y + sobelSizeV)).rgb;
+  vec3 topRight    = texture(previous_pass, vec2(uv.x + sobelSizeH, uv.y + sobelSizeV)).rgb;
+  vec3 bottomLeft  = texture(previous_pass, vec2(uv.x - sobelSizeH, uv.y - sobelSizeV)).rgb;
+  vec3 bottomRight = texture(previous_pass, vec2(uv.x + sobelSizeH, uv.y - sobelSizeV)).rgb;
+  vec3 sx = -topLeft + -2 * left + -bottomLeft + topRight   + 2 * right  + bottomRight;
+  vec3 sy = -topLeft + -2 * top  + -topRight   + bottomLeft + 2 * bottom + bottomRight;
+  color = vec4(sqrt(sx * sx + sy * sy), 1.0);
 }
