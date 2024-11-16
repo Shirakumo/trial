73bb7af1daae928d116448456a184a5b8f6a7d3e
Yukari Hafner <shinmera@tymoon.eu>
2024-11-16 12:10:45 +0100

    Fix color adjustment filter

--- data/post/color-adjustment.glsl
+++ data/post/color-adjustment.glsl
@@ -16,6 +16,11 @@ float luminance(vec3 color) {
   return dot(color, vec3(0.299f, 0.587f, 0.114f));
 }
 
+const mat3 XYZ_2_LMS_MAT =
+  mat3(+0.7328, -0.7036, +0.0030,
+       +0.4296, +1.6975, +0.0136,
+       -0.1624, +0.0061, +0.9834);
+
 const mat3 LIN_2_LMS_MAT = 
   mat3(3.90405e-1, 5.49941e-1, 8.92632e-3,
        7.08416e-2, 9.63172e-1, 1.35775e-3,
@@ -23,27 +28,20 @@ const mat3 LIN_2_LMS_MAT =
 
 const mat3 LMS_2_LIN_MAT = 
   mat3(+2.85847e+0, -1.62879e+0, -2.48910e-2,
-       -2.10182e-1,  1.15820e+0,  3.24281e-4,
-       -4.18120e-2, -1.18169e-1,  1.06867e+0);
+       -2.10182e-1, +1.15820e+0, +3.24281e-4,
+       -4.18120e-2, -1.18169e-1, +1.06867e+0);
 
 vec3 white_balance(vec3 col, float temp, float tint) {
-  float t1 = temp * 10.0f / 6.0f;
-  float t2 = tint * 10.0f / 6.0f;
+  float t1 = temp * (10.0f / 6.0f);
+  float t2 = tint * (10.0f / 6.0f);
 
   float xf = (t1 < 0) ? 0.1 : 0.05;
   float x = 0.31271 - (t1 * xf);
-  float standardIlluminantY = 2.87 * x - 3 * x * x - 0.27509507;
+  float standardIlluminantY = 2.87 * x - (3 * x * x + 0.27509507);
   float y = standardIlluminantY + t2 * 0.05;
 
   vec3 w1 = vec3(0.949237, 1.03542, 1.08728);
-
-  float Y = 1;
-  float X = Y * x / y;
-  float Z = Y * (1 - x - y) / y;
-  float L = 0.7328 * X + 0.4296 * Y - 0.1624 * Z;
-  float M = -0.7036 * X + 1.6975 * Y + 0.0061 * Z;
-  float S = 0.0030 * X + 0.0136 * Y + 0.9834 * Z;
-  vec3 w2 = vec3(L, M, S);
+  vec3 w2 = XYZ_2_LMS_MAT * vec3(x/y, 1, (1 - (x + y)) / y);
 
   vec3 balance = vec3(w1.x / w2.x, w1.y / w2.y, w1.z / w2.z);
 
@@ -65,7 +63,7 @@ void main(){
   col = max(vec3(0), white_balance(col, temperature, tint));
   col = max(vec3(0), contrast * (col - midpoint) + midpoint + brightness);
   col = max(vec3(0), col * color_filter);
-  col = max(vec3(0), mix(col, saturation, luminance(col)));
+  col = max(vec3(0), mix(vec3(luminance(col)), col, saturation));
   col = hue_shift(col, hue);
 
   color.rgb = col;
