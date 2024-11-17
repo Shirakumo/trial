7e533c69d7d74e10f6bcb59af6218fa35e2d07e6
Yukari Hafner <shinmera@tymoon.eu>
2024-11-17 09:40:21 +0100

    Minor

--- data/tone-map/hdr-tone-mapping.glsl
+++ data/tone-map/hdr-tone-mapping.glsl
@@ -8,13 +8,9 @@ float color_luminance(vec3 color){
   return dot(color, vec3(0.299, 0.587, 0.114));
 }
 
-vec3 tone_map(vec3 color){
-  if(next_method_p) return call_next_method();
-  else return color;
-}
+vec3 tone_map(vec3 color);
 
 void main(){
-  vec3 mapped = tone_map(texture(previous_pass, uv).xyz);
-  mapped = clamp(mapped, vec3(0), vec3(1));
-  color = vec4(pow(mapped.xyz, vec3(inv_gamma)), 1.0);
+  vec3 mapped = clamp(tone_map(texture(previous_pass, uv).xyz), vec3(0), vec3(1));
+  color = vec4(pow(mapped, vec3(inv_gamma)), 1.0);
 }
