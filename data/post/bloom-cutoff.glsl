1bd96c4aa7555a6a3fb1fdea36514a3596933a17
Yukari Hafner <shinmera@tymoon.eu>
2025-01-13 11:53:12 +0100

    Make the bloom a linear ramp rather than a cutoff

--- data/post/bloom-cutoff.glsl
+++ data/post/bloom-cutoff.glsl
@@ -7,9 +7,7 @@ float color_luminance(vec3 color){
 
 vec4 post_process(sampler2D previous_pass, vec2 uv){
   vec4 color = texture(previous_pass, uv);
-  float sum = color_luminance(color.xyz);
-  if(sum <= threshold)
-    return vec4(0,0,0,color.a);
-  color.rgb = max(vec3(0), color.rgb-threshold);
+  float luminance = color_luminance(color.xyz);
+  color.rgb *= clamp((luminance/threshold)-0.5, 0.0, 1.0);
   return color;
 }
