877aa350ceb649242b521131996e243d08a7ceb6
Yukari Hafner <shinmera@tymoon.eu>
2025-03-23 14:12:47 +0100

    Set task thread core mask

--- async.lisp
+++ async.lisp
@@ -8,6 +8,7 @@
                (bt:thread-alive-p (thread runner)))
     (setf (thread runner)
           (with-thread ("task-thread")
+            #+nx (setf (org.shirakumo.machine-state:thread-core-mask T) #b1000)
             (simple-tasks:start-runner runner)))
     (loop until (eql :running (simple-tasks:status runner))
           do (sleep 0.0001))))
