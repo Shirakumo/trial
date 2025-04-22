5a0fa21b04f741c447ee59f2b8ce7b24cfdf4601
Yukari Hafner <shinmera@tymoon.eu>
2025-04-22 11:48:05 +0200

    Try to be better about nsight.

--- renderdoc.lisp
+++ renderdoc.lisp
@@ -1,5 +1,9 @@
-#+quicklisp (ql:quickload :swank)
-#+(and asdf (not quicklisp)) (asdf:load-system :swank)
+#+quicklisp (ql:quickload '(:swank :cffi))
+#+(and asdf (not quicklisp)) (asdf:load-system '(:swank :cffi))
+
+#+(and unix sbcl)
+(trace cffi:load-foreign-library :condition-after (cffi:foreign-funcall "restore_sbcl_signals" :void))
+
 (swank:create-server :dont-close T)
 (loop until swank::*connections* do (sleep 0.1))
 (loop while swank::*connections* do (sleep 1.0))
