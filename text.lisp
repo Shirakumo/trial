fd8f0b900acd5d6303fde1cf8846eb22bfc98f5c
Yukari Hafner <shinmera@tymoon.eu>
2025-08-07 11:50:49 +0200

    Try to handle stream output in repl

--- text.lisp
+++ text.lisp
@@ -101,24 +101,27 @@ void main(){
 
 (define-shader-entity repl (debug-text listener)
   ((text :initform (make-array 4096 :adjustable T :fill-pointer T :element-type 'character))
+   (stream)
    (history :initform (make-array 32 :adjustable T :fill-pointer 0) :accessor history)
    (history-index :initform 0 :accessor history-index)
    (line-count :initform 40 :initarg :line-count :accessor line-count)
    (input-start :initform 0 :accessor input-start)))
 
-(defmethod initialize-instance :after ((repl repl) &key)
-  (format (text repl) "~%This is ~a ~a, an implementation of ANSI Common Lisp.
+(defmethod initialize-instance :after ((repl repl) &key (show-preamble T))
+  (when show-preamble
+    (format (text repl) "~%This is ~a ~a, an implementation of ANSI Common Lisp.
 as ~a @ ~a
 on ~a ~a,
 a ~a ~a machine
 with ~a ~a~%"
-          (lisp-implementation-type) (lisp-implementation-version)
-          (username T) (machine-instance)
-          (software-type) (software-version)
-          (machine-type) (machine-version)
-          +app-system+ (version :app))
+            (lisp-implementation-type) (lisp-implementation-version)
+            (username T) (machine-instance)
+            (software-type) (software-version)
+            (machine-type) (machine-version)
+            +app-system+ (version :app)))
+  (setf (slot-value repl 'stream) (make-instance 'repl-stream :repl repl))
   (setf (text repl) (text repl))
-  (output-result () repl))
+  (output-result :none repl))
 
 (define-handler (repl text-entered) (text)
   (loop for char across text do (vector-push-extend char (text repl)))
@@ -136,21 +139,25 @@ with ~a ~a~%"
          (decf (fill-pointer text))
          (setf (text repl) text)))
       ((:enter :return)
-       (if (= (input-start repl) (length text))
-           (output-result () repl)
-           (handler-case
-               (let ((start (shiftf (input-start repl) (length text))))
-                 (vector-push-extend (subseq text start) (history repl))
-                 (setf (history-index repl) (length (history repl)))
-                 (let* ((- (read-from-string text T NIL :start start))
-                        (values (multiple-value-list (eval -))))
-                   (shiftf *** ** * (first values))
-                   (shiftf /// cl:// / values)
-                   (shiftf +++ ++ + -)
-                   (output-result values repl)))
-             (error (e)
-               (format (text repl) "~%ERROR: ~a~%" e)
-               (output-result () repl))))))))
+       (cond ((= (input-start repl) (length text))
+              (output-result :none repl))
+             (T
+              ;(format (text repl) "~%")
+              (handler-case
+                  (let ((start (shiftf (input-start repl) (length text))))
+                    (vector-push-extend (subseq text start) (history repl))
+                    (setf (history-index repl) (length (history repl)))
+                    (let* ((*standard-output* (slot-value repl 'stream))
+                           (*error-output* *standard-output*)
+                           (- (read-from-string text T NIL :start start))
+                           (values (multiple-value-list (eval -))))
+                      (shiftf *** ** * (first values))
+                      (shiftf /// cl:// / values)
+                      (shiftf +++ ++ + -)
+                      (setf (input-start repl) (length text))
+                      (output-result values repl)))
+                (error (e)
+                  (output-result e repl)))))))))
 
 (defmethod (setf history-index) :around (index (repl repl))
   (call-next-method (clamp 0 index (length (history repl))) repl))
@@ -162,9 +169,22 @@ with ~a ~a~%"
     (replace (text repl) history-item :start1 (input-start repl))
     (setf (text repl) (text repl))))
 
-(defmethod output-result (values (repl repl))
+(defmethod output-result ((values (eql :none)) (repl repl)))
+
+(defmethod output-result ((values string) (repl repl))
+  (format (text repl) values))
+
+(defmethod output-result ((values cons) (repl repl))
+  (format (text repl) "~{~%~s~}" values))
+
+(defmethod output-result ((values null) (repl repl))
+  (format (text repl) "~%; No values"))
+
+(defmethod output-result ((values condition) (repl repl))
+  (format (text repl) "~%~a: ~a~%" (type-of values) values))
+
+(defmethod output-result :after (result (repl repl))
   (let ((text (text repl)))
-    (format text "~{~%~s~}" values)
     (format text "~%~a> " (package-abbreviation *package*))
     ;; Scroll lines
     (let ((to-remove (- (loop for char across text
@@ -178,3 +198,72 @@ with ~a ~a~%"
                  (decf to-remove))))
     (setf (input-start repl) (length text))
     (setf (text repl) text)))
+
+(defun repl-start-of-input-line (repl)
+  (if (= (input-start repl) (length (text repl)))
+      (input-start repl)
+      (repl-start-of-line repl (input-start repl))))
+
+(defun repl-start-of-line (repl cursor)
+  (let ((text (text repl)))
+    (if (= (input-start repl) (length text))
+        (length text)
+        (loop for i downfrom cursor above 0
+              do (when (char= #\Linefeed (aref text (1- i)))
+                   (return i))
+              finally (return 0)))))
+
+(defun repl-input-line (repl)
+  (if (= (input-start repl) (length (text repl)))
+      NIL
+      (subseq (text repl) (repl-start-of-input-line repl))))
+
+(defclass repl-stream (trivial-gray-streams:fundamental-character-output-stream)
+  ((repl :initarg :repl :initform (arg! :repl))))
+
+(defmethod trivial-gray-streams:stream-clear-output ((stream repl-stream))
+  (let* ((repl (slot-value stream 'repl))
+         (output (repl-input-line repl)))
+    (setf (fill-pointer (text repl)) 0)
+    (cond (output
+           (setf (input-start repl) (length output))
+           (format (text repl) output))
+          (T
+           (setf (input-start repl) 0)))))
+
+(defmethod trivial-gray-streams:stream-line-column ((stream repl-stream))
+  (let* ((repl (slot-value stream 'repl))
+         (start (repl-start-of-input-line repl)))
+    (if (= 0 start)
+        0
+        (- (1- start)
+           (repl-start-of-line repl (1- start))))))
+
+(defmethod trivial-gray-streams:stream-terpri ((stream repl-stream))
+  (trivial-gray-streams:stream-write-char stream #\Linefeed))
+
+(defmethod trivial-gray-streams:stream-write-char ((stream repl-stream) char)
+  (let* ((repl (slot-value stream 'repl))
+         (output (repl-input-line repl)))
+    (cond (output
+           (decf (fill-pointer (text repl)) (1+ (length output)))
+           (format (text repl) "~c~%~a" char output))
+          (T
+           (format (text repl) "~c" char)
+           (setf (input-start repl) (length (text repl)))))))
+
+(defmethod trivial-gray-streams:stream-write-string ((stream repl-stream) string &optional (start 0) (end (length string)))
+  (let* ((repl (slot-value stream 'repl))
+         (string (if (or (/= start 0) (/= end (length string)))
+                     (subseq string start end)
+                     string))
+         (output (repl-input-line repl)))
+    (cond (output
+           (decf (fill-pointer (text repl)) (1+ (length output)))
+           (format (text repl) "~a~%~a" string output))
+          (T
+           (format (text repl) "~a" string)
+           (setf (input-start repl) (length (text repl)))))))
+
+(defmethod trivial-gray-streams:stream-write-sequence ((stream repl-stream) (string string) start end &key)
+  (trivial-gray-streams:stream-write-string stream string start end))
